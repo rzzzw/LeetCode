e89eb15d950e4fd9754b9d771073a88e65b1b8d0
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-09-29 20:26:01 -0700

    Time: 0 ms (100.00%), Space: 39.1 MB (61.16%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,24 +1,12 @@
 class Solution {
     public int numTrees(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        // if (n == 2) return 2;
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n + 1]; 
         dp[0] = 1;
         dp[1] = 1;
-        // dp[2] = 2;
-
-        return helper(n, dp);
-    }
-
-    private int helper(int n, int[] dp) {
-        if (dp[n] != 0) {
-            return dp[n];
-        }
-        
-        for (int i = 1; i <= n; i++) { // root from 1 ~ n
-            dp[n] += helper(i - 1, dp) * helper(n - i, dp);
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
         }
         return dp[n];
     }
@@ -26,79 +14,23 @@ class Solution {
 
 /**
 
-WIF input = 5
-    output = root(1) + root(2) + root(3) + root(4) + root(5)
-    
-            root(1)                  root(2)               root(3)               root(4)            root(5)
-            /      \                 /      \              /      \              /      \           /      \
-        0 Node     4 Node       1 Node      3 Node     2 Node     2 Node    3 Node     1 Node    4 Node    0 Node
-        left sub   right sub    
+1 node => dp[1] = 1
+2 nodes => dp[2] = 2
+        example: a < b       b      a
+                            /   or   \
+                           a          b
+                   
+        1' left 0, right 1  => dp[0] * dp[1] = 1
+        2' left 1, right 0  => dp[1] * dp[0] = 1
+        ( 0 node represents null subtree => let dp[0] = 1 )
+        
+   dp[2] = 2     
 
-
-             root(1) 
-            /      \
-      0 Node        4 Node
-      ~~~~~~~       ~~~~~~~~~  
-    null => 1       Output = root(2) + root(3) + root(4) + root(5)
-
-                    root(2)
-                /           \
-            0 Node        3 Node
-            ~~~~~~~       ~~~~~~~~~  
-          null => 1       Output = root(3) + root(4) + root(5)
-
-                                root(3)
-                            /           \
-                        0 Node        2 Node
-                        ~~~~~~~       ~~~~~~~~~  
-                      null => 1       Output = root(4) + root(5)
-                                                ~~~~~~~~~~~~~~ 2 nodes has only 2 options 
-                                                4            5
-                                                 \    or    /
-                                                  5        4
-
-Base case:
-    subtree 0 node -> 1 unique structure
-    subtree 1 node -> 1 unique structure
-    subtree 2 nodes -> 2 unique structures
-
-Recursion rule:
-    root node = # of left subtree * # of right subtree
-
-
-Visualized examples -> input 5, all structures with root = 1:
-                                                                                           
- 
-            1                1                     1                        1                       1             |                  
-        /       \         /     \              /       \                /      \                 /     \          |         
-    null         2     null      2          null        2             null       2             null     2         |   
-               /   \               \                  /    \                   /   \                  /    \      |    
-           null     3               3             null     4                null     5             null     5     |             
-                  /   \               \                  /   \                     /                       /      |                  
-              null     4               5                3     5                   3                      4        |                               
-                      /  \            /                                             \                   /         |                          
-                  null    5          4                                               4                 3          |        
-
-
-                                        
-            1             1           
-        /     \         /    \       
-      null     3     null     3    
-             /   \          /  \        
-            2     4        2    5        
-                   \           /          
-                    5         4             
-                                     
-                                   
-
-            1                 1               1              1               1              1            1
-         /     \            /    \         /      \      /       \       /       \      /       \     /       \
-       null     4       null      4      null      5    null      5   null       5    null       5  null       5 
-             /    \            /    \            /              /                /               /             /
-            2      5          3      5         2               2                3               4             4
-             \               /                  \               \             /   \            /             /
-              3             2                     3               4          2     4          2             3
-                                                   \             /                             \           /
-                                                    4           3                               3         2                                              
+3 nodes:
+        1' left 0, right 2  => dp[0] * dp[2] = 2
+        2' left 1, right 1  => dp[1] * dp[1] = 1
+        3' left 2, right 0  => dp[2] * dp[0] = 2
+        
+   dp[3] = 5
 
  */
