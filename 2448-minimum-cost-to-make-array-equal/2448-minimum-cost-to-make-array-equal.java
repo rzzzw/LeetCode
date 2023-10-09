2998720aa5fce3c3cfff5f8c10ca8d9d5119ec6f
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-10-09 16:23:47 -0700

    Time: 18 ms (45.11%), Space: 54.6 MB (77.09%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -9,33 +9,28 @@ class Solution {
         
         Arrays.sort(pairs, (a, b) -> a[0] - b[0]);        
         
-        // idx      0  1  2  3
-        // num      1  2  3  5
-        // cost     2 14  3  1
-        // pref_L   2 16 19
-        // left        2 18 18+19*2=56
-        
-        
-        long[] left = new long[n];
-        long preSumLeft = pairs[0][1];
-        for (int i = 1; i < n; i++) {
-            left[i] = left[i - 1] + preSumLeft * (pairs[i][0] - pairs[i - 1][0]);
-            preSumLeft += pairs[i][1];
+        // treat cost as the repeat qty of nums
+        long totalCount = 0;
+        for (int i : cost) {
+            totalCount += i;
         }
         
-        long[] right = new long[n];
-        long preSumRight = pairs[n - 1][1];
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = right[i + 1] + preSumRight * (pairs[i + 1][0] - pairs[i][0]);
-            preSumRight += pairs[i][1];
-        }
-        
-        long res = right[0] + left[0];
+        long count = 0;
+        int idx = -1;
         for (int i = 0; i < n; i++) {
-            if (right[i] + left[i] < res) {
-                res = right[i] + left[i];
-            }            
+            count += pairs[i][1];
+            if (count >= totalCount * 1.0 / 2) {
+                idx = i;
+                break;
+            }
         }
-        return res;
+        
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            res += Math.abs(pairs[i][0] - pairs[idx][0]) * 1.0 * pairs[i][1];
+        }
+        
+        return res;        
+        
     }
 }
