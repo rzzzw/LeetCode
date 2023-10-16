fdd434e19215196ec6a8b1c97fca034e4a50f0c1
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-10-15 22:18:22 -0700

    Time: 6 ms (65.88%), Space: 43.2 MB (88.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,33 +3,34 @@ class Solution {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        // char[] input = s.toCharArray();
-        // Map<Character, Integer> map = new HashMap<>();
-        // int longest = 0;
-        // int i = 0;
-        // for (int j = 0; j < input.length; j++) {
-        //     if (map.containsKey(input[j])) {
-        //         i = Math.max(map.get(input[j]) + 1, i);
-        //     }
-        //     longest = Math.max(longest, j - i + 1);
-        //     map.put(input[j], j);
-        // }
-        // return longest;
-        
-        Set<Character> distinct = new HashSet<>();
-        int slow = 0;
-        int fast = 0;
+        char[] input = s.toCharArray();
+        Map<Character, Integer> distinct = new HashMap<>();
         int longest = 0;
-        while (fast < s.length()) {
-            if (distinct.contains(s.charAt(fast))){
-                distinct.remove(s.charAt(slow));
+        int slow = 0;
+        for (int fast = 0; fast < input.length; fast++) {
+            while (distinct.containsKey(input[fast])) {
+                distinct.remove(input[slow]);
                 slow++;
-            } else {
-                distinct.add(s.charAt(fast));
-                fast++;
-                longest = Math.max(longest, fast - slow);
             }
+            longest = Math.max(longest, fast - slow + 1);
+            distinct.put(input[fast], fast);
         }
         return longest;
+        
+        // Set<Character> distinct = new HashSet<>();
+        // int slow = 0;
+        // int fast = 0;
+        // int longest = 0;
+        // while (fast < s.length()) {
+        //     if (distinct.contains(s.charAt(fast))){
+        //         distinct.remove(s.charAt(slow));
+        //         slow++;
+        //     } else {
+        //         distinct.add(s.charAt(fast));
+        //         fast++;
+        //         longest = Math.max(longest, fast - slow);
+        //     }
+        // }
+        // return longest;
     }
 }
