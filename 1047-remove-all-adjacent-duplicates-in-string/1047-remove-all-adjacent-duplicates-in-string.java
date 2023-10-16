506a325dfeae392a0a2c225adc4b4fcfbd884265
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-10-15 21:06:15 -0700

    Time: 9 ms (93.67%), Space: 43.7 MB (98.94%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,21 +1,33 @@
 class Solution {
     public String removeDuplicates(String s) {
-        if (s == null || s.length() == 0) {
-            return s;
-        }
-        Deque<Character> stack = new ArrayDeque<>();
-        for (char c : s.toCharArray()) {
-            if (!stack.isEmpty() && stack.peekLast() == c) {
-                stack.removeLast();
-            } else {
-                stack.addLast(c);
-            }
-        }
+//         if (s == null || s.length() == 0) {
+//             return s;
+//         }
+//         Deque<Character> stack = new ArrayDeque<>();
+//         for (char c : s.toCharArray()) {
+//             if (!stack.isEmpty() && stack.peekLast() == c) {
+//                 stack.removeLast();
+//             } else {
+//                 stack.addLast(c);
+//             }
+//         }
 
+//         StringBuilder sb = new StringBuilder();
+//         while (!stack.isEmpty()) {
+//             char cur = stack.removeFirst();
+//             sb.append(cur);      
+//         }
+//         return sb.toString();
         StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            char cur = stack.removeFirst();
-            sb.append(cur);      
+        int sbLen = 0;
+        for (char c : s.toCharArray()) {
+            if (sbLen != 0 && sb.charAt(sbLen - 1) == c) {
+                sb.deleteCharAt(sbLen - 1);
+                sbLen--;
+            } else {
+                sb.append(c);
+                sbLen++;
+            }
         }
         return sb.toString();
     }
