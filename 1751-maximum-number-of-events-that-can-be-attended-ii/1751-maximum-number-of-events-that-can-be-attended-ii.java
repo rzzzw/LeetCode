8508006f101717c974a4bf7cca504252f568f3bd
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-10-28 21:52:11 -0700

    Time: 116 ms (14.12%), Space: 118.7 MB (68.29%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, Comparator.comparingInt(a -> a[1])); // Sort by ending points
+        Arrays.sort(events, (a, b) -> a[1] - b[1]); // Sort by ending points
         int n = events.length;
         int[][] dp = new int[n+1][k+1];
         int res = 0;
