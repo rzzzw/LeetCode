7c77ec59d9734a06760baaa6f48e3a1eccfcf21b
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-11-01 19:38:36 -0700

    Time: 0 ms (100.00%), Space: 40.6 MB (65.02%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -16,16 +16,70 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        postorderTraversal(root, res);
-        return res;
-    }
-    
-    private void postorderTraversal(TreeNode root, List<Integer> res) {
-        if (root == null) {
-            return;
+        if (root == null) return res;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode prev = null;
+        stack.offerFirst(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.peekFirst();
+            if (prev == null || cur == prev.left || cur == prev.right) {
+                if (cur.left != null) {
+                    stack.offerFirst(cur.left);
+                } else if (cur.right != null) {
+                    stack.offerFirst(cur.right);
+                } else {
+                    res.add(cur.val);
+                    stack.pollFirst();
+                }
+            } else if (prev == cur.left) {
+                if (cur.right != null) {
+                    stack.offerFirst(cur.right);
+                } else {
+                    res.add(cur.val);
+                    stack.pollFirst();
+                }
+                
+            } else {
+                res.add(cur.val);
+                stack.pollFirst();
+            }
+            prev = cur;
         }
-        postorderTraversal(root.left, res);
-        postorderTraversal(root.right, res);
-        res.add(root.val);
+        return res;
+
     }
 }
+
+/*
+     1
+    / \
+   2   3
+  / \
+ 4   5
+
+stack: 1 2
+prev: 5
+cur: 2
+
+res: 4 5
+
+*/
+
+// recursion:
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         postorderTraversal(root, res);
+//         return res;
+//     }
+    
+//     private void postorderTraversal(TreeNode root, List<Integer> res) {
+//         if (root == null) {
+//             return;
+//         }
+//         postorderTraversal(root.left, res);
+//         postorderTraversal(root.right, res);
+//         res.add(root.val);
+//     }
+// }
