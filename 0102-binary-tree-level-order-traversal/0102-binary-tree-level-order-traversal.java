8e2d5d18541f101435a56723cc8b3e19229847a4
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-11-02 14:20:58 -0700

    Time: 0 ms (100.00%), Space: 43.6 MB (90.32%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -17,23 +17,32 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
-        Queue<TreeNode> q = new ArrayDeque<>();
-        q.offer(root);
-        while(!q.isEmpty()) {
-            List<Integer> curLayer = new ArrayList<>();
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode cur = q.poll();
-                curLayer.add(cur.val);
-                if (cur.left != null) {
-                    q.offer(cur.left);
-                } 
-                if (cur.right != null) {
-                    q.offer(cur.right);
-                }
-            }
-            res.add(curLayer);
-        }
+        helper(root, 0, res);
         return res;
     }
+    
+    private void helper(TreeNode node, int level, List<List<Integer>> res) {
+        if (res.size() == level) {
+            res.add(new ArrayList<Integer>());
+        }
+        res.get(level).add(node.val);
+        
+        if (node.left != null) {
+            helper(node.left, level + 1, res);
+        }
+        if (node.right != null) {
+            helper(node.right, level + 1, res);
+        }
+    }
 }
+
+/*
+        1
+      /   \
+     2     3
+    / \
+   4   5
+   
+[[1][2][4, 5]]
+
+*/
