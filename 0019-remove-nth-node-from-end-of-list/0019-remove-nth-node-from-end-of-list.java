d9b18f70e1de5dc5793ea873c0952b60f8ed3409
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-11-03 14:40:26 -0700

    Time: 0 ms (100.00%), Space: 40.5 MB (52.94%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -8,35 +8,72 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// traverse 1 time:
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) {
-            return null;
-        }
-        int size = 0;
-        ListNode cur = head;
-        // traverse the list to get the size.
-        while (cur != null) {
-            size++;
-            cur = cur.next;
-        }
-        
-        // traverse the list again to remove the required node
+        if (head == null) return null;
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        cur = dummy;
+        ListNode left = dummy;
+        ListNode right = dummy;
         
-        for (int i = 0; i < size - n; i++) {
-            cur = cur.next;
+        for (int i = 0; i <= n; i++) {
+            right = right.next;
         }
-        cur.next = cur.next.next;
+        
+        while (right != null) {
+            right = right.next;
+            left = left.next;
+        }
+        
+        left.next = left.next.next;
         return dummy.next;
-    }
+    }   
 }
 
 /*
-len = 5 ==> idx = 5 - 2 = 3
-TC: O(2L) ~ O(L)
-SC: O(1)
-
+    dummy head
+     __    1   2   3   4   5
+     l ->
+               r ->
+     |--- 2 ---|         
+     
+     l and r move in the same direction at the same time, with an interval of n.
 */
+
+
+
+// traverse 2 times: 
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         if (head == null) {
+//             return null;
+//         }
+//         int size = 0;
+//         ListNode cur = head;
+//         // traverse the list to get the size.
+//         while (cur != null) {
+//             size++;
+//             cur = cur.next;
+//         }
+        
+//         // traverse the list again to remove the required node
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+//         cur = dummy;
+        
+//         for (int i = 0; i < size - n; i++) {
+//             cur = cur.next;
+//         }
+//         cur.next = cur.next.next;
+//         return dummy.next;
+//     }
+// }
+
+// /*
+// len = 5 ==> idx = 5 - 2 = 3
+// TC: O(2L) ~ O(L)
+// SC: O(1)
+
+// */
