f51151f8921bf5758ee93e4febaef1be632da0a3
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-11-03 23:27:09 -0700

    Time: 1 ms (23.27%), Space: 43.7 MB (67.21%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null || root.left == null && root.right == null) {
+            return true;
+        }
+        BSTInfo res = helper(root);
+        return res.isBST;
+    }
+    
+    private BSTInfo helper(TreeNode node) {
+        if (node == null) {
+            return new BSTInfo(true, Long.MAX_VALUE, Long.MIN_VALUE);
+        } 
+        
+        BSTInfo left = helper(node.left);
+        BSTInfo right = helper(node.right);
+        
+        if (left.isBST && right.isBST && node.val > left.max && node.val < right.min) {
+            long min = (node.left == null) ? node.val : left.min;
+            long max = (node.right == null) ? node.val : right.max;
+            return new BSTInfo(true, min, max);
+        } 
+        return new BSTInfo(false, 0, 0);
+        
+    }
+    
+    class BSTInfo {
+        boolean isBST;
+        long min;
+        long max;
+        
+        BSTInfo(boolean isBST, long min, long max) {
+            this.isBST = isBST;
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
