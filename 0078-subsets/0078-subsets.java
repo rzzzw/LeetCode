c4acbfb2bc06bb7b39e814748fe17d895696cc5f
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-11-04 15:09:39 -0700

    Time: 1 ms (70.57%), Space: 41.5 MB (77.77%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -9,37 +9,83 @@ class Solution {
     }
     
     private void backTracking(int[] nums, int idx, List<Integer> cur, List<List<Integer>> res) {
-        if (idx == nums.length) {
-            res.add(new ArrayList<>(cur));
-            return;
-        }
-        cur.add(nums[idx]);
-        backTracking(nums, idx+1, cur, res);
-        cur.remove(cur.size() - 1);
+        res.add(new ArrayList<>(cur));
         
-        backTracking(nums, idx+1, cur, res);
+        for (int i = idx; i < nums.length; i++) {
+            cur.add(nums[i]);
+            backTracking(nums, i + 1, cur, res);
+            cur.remove(cur.size() - 1);
+        }
     }
 }
 
-
-/**
-Time complexity: 2^n
-
-                                  _                                                 2^0
-                        /                     \
-                    add                          not_add                            2^1
-               /           \                 /            \ 
-            add        not_add            add            not_add                    2^2
-         /    \        /     \           /     \         /     \                    
-     add    not_add   add   not_add    add   not_add   add     not_add              2^3
-
-                                                                                    2^n
-
- 2^0 + 2^1 + 2^2 + ... + 2^n = 2 * 2^(n-1) - 1
+/*
+Recursion Tree
+									                     [		]
+size = 0  solution
+								                    /		\		   \
+ level 1:                                        1			  2		      3
+size = 1  solution
+									          /	  \ 	    	\
+	level 2:						       1,2	 1,3	        2,3
+size = 2  solution		
+								          /
+	level3:						      1,2,3
+size = 3  solution					                     
 
 
-Space complexity: 2^n * n
-    call stack: O(height) -> n
-    Lists(res & cur): 2^n * n 
+[],[1],[1,2],[1,2,3],[1,3],[2],[2,3],[3]
 
- */
+Time: O(2^n)
+
+*/
+
+
+
+
+//// Add or not add:
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if (nums == null || nums.length == 0) {
+//             return res;
+//         }
+//         backTracking(nums, 0, new ArrayList<Integer>(), res);
+//         return res;
+//     }
+    
+//     private void backTracking(int[] nums, int idx, List<Integer> cur, List<List<Integer>> res) {
+//         if (idx == nums.length) {
+//             res.add(new ArrayList<>(cur));
+//             return;
+//         }
+//         cur.add(nums[idx]);
+//         backTracking(nums, idx+1, cur, res);
+//         cur.remove(cur.size() - 1);
+        
+//         backTracking(nums, idx+1, cur, res);
+//     }
+// }
+
+
+// /**
+// Time complexity: 2^n
+
+//                                   _                                                 2^0
+//                         /                     \
+//                     add                          not_add                            2^1
+//                /           \                 /            \ 
+//             add        not_add            add            not_add                    2^2
+//          /    \        /     \           /     \         /     \                    
+//      add    not_add   add   not_add    add   not_add   add     not_add              2^3
+
+//                                                                                     2^n
+
+//  2^0 + 2^1 + 2^2 + ... + 2^n = 2 * 2^(n-1) - 1
+
+
+// Space complexity: 2^n * n
+//     call stack: O(height) -> n
+//     Lists(res & cur): 2^n * n 
+
+//  */
