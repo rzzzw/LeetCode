c240f0b377e66a07a8fbf7b7d83256429bb1fe99
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-11-03 23:37:36 -0700

    Time: 0 ms (100.00%), Space: 43.6 MB (78.86%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,41 +13,60 @@
  *     }
  * }
  */
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if (root == null || root.left == null && root.right == null) {
+        return isBST(root, null, null);
+    }
+    
+    private boolean isBST(TreeNode root, Integer min, Integer max) {
+        if (root == null) {
             return true;
         }
-        BSTInfo res = helper(root);
-        return res.isBST;
-    }
-    
-    private BSTInfo helper(TreeNode node) {
-        if (node == null) {
-            return new BSTInfo(true, Long.MAX_VALUE, Long.MIN_VALUE);
-        } 
-        
-        BSTInfo left = helper(node.left);
-        BSTInfo right = helper(node.right);
-        
-        if (left.isBST && right.isBST && node.val > left.max && node.val < right.min) {
-            long min = (node.left == null) ? node.val : left.min;
-            long max = (node.right == null) ? node.val : right.max;
-            return new BSTInfo(true, min, max);
-        } 
-        return new BSTInfo(false, 0, 0);
-        
-    }
-    
-    class BSTInfo {
-        boolean isBST;
-        long min;
-        long max;
-        
-        BSTInfo(boolean isBST, long min, long max) {
-            this.isBST = isBST;
-            this.min = min;
-            this.max = max;
+        if ((min != null && root.val <= min) || (max != null && root.val >= max)) {
+            return false;
         }
+        return isBST(root.left, min, root.val) && isBST(root.right, root.val, max);
     }
 }
+
+
+// // Bottom-up
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         if (root == null || root.left == null && root.right == null) {
+//             return true;
+//         }
+//         BSTInfo res = helper(root);
+//         return res.isBST;
+//     }
+    
+//     private BSTInfo helper(TreeNode node) {
+//         if (node == null) {
+//             return new BSTInfo(true, Long.MAX_VALUE, Long.MIN_VALUE);
+//         } 
+        
+//         BSTInfo left = helper(node.left);
+//         BSTInfo right = helper(node.right);
+        
+//         if (left.isBST && right.isBST && node.val > left.max && node.val < right.min) {
+//             long min = (node.left == null) ? node.val : left.min;
+//             long max = (node.right == null) ? node.val : right.max;
+//             return new BSTInfo(true, min, max);
+//         } 
+//         return new BSTInfo(false, 0, 0);
+        
+//     }
+    
+//     class BSTInfo {
+//         boolean isBST;
+//         long min;
+//         long max;
+        
+//         BSTInfo(boolean isBST, long min, long max) {
+//             this.isBST = isBST;
+//             this.min = min;
+//             this.max = max;
+//         }
+//     }
+// }
