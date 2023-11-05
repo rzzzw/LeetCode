6c393e46eccb29731331b302e8a42833a8534761
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-11-04 20:24:40 -0700

    Time: 14 ms (84.66%), Space: 39.7 MB (51.01%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -2,11 +2,14 @@ class Solution {
     public int distributeCookies(int[] cookies, int k) {
         int[] distribution = new int[k]; 
         int[] minimumMax = new int[]{Integer.MAX_VALUE};
-        dfs(cookies, k, 0, distribution, minimumMax);
+        dfs(cookies, k, k, 0, distribution, minimumMax);
         return minimumMax[0];
     }
 
-    private void dfs(int[] cookies, int k, int curCookieIdx, int[] distribution, int[] minimumMax) {
+    private void dfs(int[] cookies, int k, int childNoCookie, int curCookieIdx, int[] distribution, int[] minimumMax) {
+        if (cookies.length - curCookieIdx < childNoCookie) {
+            return;
+        }
         if (curCookieIdx == cookies.length) {
             int max = 0;
             for (int i : distribution) {
@@ -17,12 +20,18 @@ class Solution {
         }
         
         for (int i = 0; i < k; i++) {
+            childNoCookie -= distribution[i] == 0 ? 1 : 0;
             distribution[i] += cookies[curCookieIdx];
-            dfs(cookies, k, curCookieIdx + 1, distribution, minimumMax);
+            
+            dfs(cookies, k, childNoCookie, curCookieIdx + 1, distribution, minimumMax);
+            
             distribution[i] -= cookies[curCookieIdx];
+            childNoCookie += distribution[i] == 0 ? 1 : 0;
         }
     }
 }
+
+// TC： O(k^n)
 
 
 
@@ -32,8 +41,8 @@ class Solution {
 //         return dfs(0, distribute, cookies, k, k);
 //     }
 
-//     private int dfs(int i, int[] distribute, int[] cookies, int k, int zeroCount) {
-//         if (cookies.length - i < zeroCount) {
+//     private int dfs(int i, int[] distribute, int[] cookies, int k, int childNoCookie) { 
+//         if (cookies.length - i < childNoCookie) {
 //             return Integer.MAX_VALUE;
 //         }
 
@@ -47,14 +56,41 @@ class Solution {
 
 //         int answer = Integer.MAX_VALUE;
 //         for (int j = 0; j < k; j++) {
-//             zeroCount -= distribute[j] == 0 ? 1 : 0;
+//             childNoCookie -= distribute[j] == 0 ? 1 : 0;
 //             distribute[j] += cookies[i];
 
-//             answer = Math.min(answer, dfs(i + 1, distribute, cookies, k, zeroCount));
+//             answer = Math.min(answer, dfs(i + 1, distribute, cookies, k, childNoCookie));
 
 //             distribute[j] -=cookies[i];
-//             zeroCount += distribute[j] == 0 ? 1 : 0;
+//             childNoCookie += distribute[j] == 0 ? 1 : 0;
 //         }
 //         return answer;
+//     }
+// }
+
+
+// class Solution {
+//     public int distributeCookies(int[] cookies, int k) {
+//         int[] distribution = new int[k]; 
+//         int[] minimumMax = new int[]{Integer.MAX_VALUE};
+//         dfs(cookies, k, 0, distribution, minimumMax);
+//         return minimumMax[0];
+//     }
+
+//     private void dfs(int[] cookies, int k, int curCookieIdx, int[] distribution, int[] minimumMax) {
+//         if (curCookieIdx == cookies.length) {
+//             int max = 0;
+//             for (int i : distribution) {
+//                 max = Math.max(max, i);
+//             }
+//             minimumMax[0] = Math.min(minimumMax[0], max);
+//             return;
+//         }
+        
+//         for (int i = 0; i < k; i++) {
+//             distribution[i] += cookies[curCookieIdx];
+//             dfs(cookies, k, curCookieIdx + 1, distribution, minimumMax);
+//             distribution[i] -= cookies[curCookieIdx];
+//         }
 //     }
 // }
