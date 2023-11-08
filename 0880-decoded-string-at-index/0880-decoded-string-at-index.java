982b83f74699a313c82733cf418d484ab1abe0e9
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2023-11-07 23:09:38 -0800

    Time: 0 ms (100.00%), Space: 40.6 MB (15.31%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String decodeAtIndex(String s, int k) {
+        long size = 0;
+        int N = s.length();
+        for (int i = 0; i < N; i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                size *= c - '0';
+            } else {
+                size++;
+            }
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            k %= size;
+            if (k == 0 && Character.isLetter(c)) {
+                return Character.toString(c);
+            }
+            if (Character.isDigit(c)) {
+                size /= c-'0';
+            } else {
+                size--;
+            }
+        }
+        throw null;
+        // return null;
+    }
+}
+
+/*
+leet2code3
+leetleetcodeleetleetcodeleetleetcode
+1234567890
+
+ha22
+hahahaha
+12345
+
+
+*/
