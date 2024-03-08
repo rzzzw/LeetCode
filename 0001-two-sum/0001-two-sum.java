a0bf019d7540aa5403311c44e1a17090225ab0ac
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2024-03-08 09:52:32 -0800

    Time: 2 ms (97.98%), Space: 44.9 MB (32.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,10 +4,20 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int x = target - nums[i];
             if (map.containsKey(x)) {
-                return new int[]{i, map.get(x)};
+                return new int[]{map.get(x), i};
             }
             map.put(nums[i], i);
         }
         return null;
-    }
+    } 
 }
+
+/*
+target = 9
+
+ i:      0 1 2  3
+num[i]:  2 7 11 15
+ x:      7 2
+
+map: {{2, 0}}
+*/
