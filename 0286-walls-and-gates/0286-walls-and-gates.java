f13c3eda90ba1a3546d56571590452af66c5a08b
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2024-03-29 15:55:56 -0700

    Time: 9 ms (62.93%), Space: 47.8 MB (51.44%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.java
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -16,20 +16,17 @@ class Solution {
             int size = q.size();
             for (int i = 0; i < size; i++) {
                 int[] cur = q.poll();
-                int row = cur[0];
-                int col = cur[1];
-                // rooms[row][col] = Math.min(rooms[row][col], step);
                 
                 for (int[] dir : dirs){
-                    int x = row + dir[0];
-                    int y = col + dir[1];
+                    int x = cur[0] + dir[0];
+                    int y = cur[1] + dir[1];
                     // Case 1 - out of boundry, handle with continue to skip
                     if (x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length) {
                         continue; 
                     }
                     
                     // Case 2 - cell value != Integer.MAX_VALUE(2^31-1), do nothing, don't offer to the queue
-                    //      2.1: cell value == 0, all are considered initially, they will keep as 0 step to the nearest gate
+                    //      2.1: cell value == 0, are start cells in 2D BFS, value no change
                     //      2.2: cell value == -1(A wall or an obstacle), keep as -1
                     //      2.3: cell value == any other number, means already updated with a smallest step value in previous traverse.
                     if (rooms[x][y] != Integer.MAX_VALUE) {
