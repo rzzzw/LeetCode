8e12124658821c0172771f6bc0a71096c263217f
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2024-03-29 15:41:26 -0700

    Time: 115 ms (11.46%), Space: 169.5 MB (5.12%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.java
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -9,24 +9,32 @@ class Solution {
             }
         }
         
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-              
+        int[][] dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        
         int step = 0;
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
                 int[] cur = q.poll();
                 int row = cur[0];
                 int col = cur[1];
-                rooms[row][col] = Math.min(step, rooms[row][col]);
+                rooms[row][col] = Math.min(rooms[row][col], step);
                 
-                for (int[] dir : dirs) {
-                    int x = dir[0] + row;
-                    int y = dir[1] + col;
+                for (int[] dir : dirs){
+                    int x = row + dir[0];
+                    int y = col + dir[1];
+                    // Case 1 - out of boundry, handle with continue to skip
+                    if (x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length) {
+                        continue; 
+                    }
                     
-                    if (x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length) continue;
-                    if (rooms[x][y] != Integer.MAX_VALUE) continue;
-                    
+                    // Case 2 - cell value != Integer.MAX_VALUE(2^31-1), do nothing, don't offer to the queue
+                    //      2.1: cell value == 0, all are considered initially, they will keep as 0 step to the nearest gate
+                    //      2.2: cell value == -1(A wall or an obstacle), keep as -1
+                    //      2.3: cell value == any other number, means already updated with a smallest step value in previous traverse.
+                    if (rooms[x][y] != Integer.MAX_VALUE) {
+                        continue;
+                    }
                     q.offer(new int[]{x, y});
                 }
             }
