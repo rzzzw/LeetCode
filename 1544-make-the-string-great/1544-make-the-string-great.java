560e283756ef121b126cd5b1cbcde37ecc761eb8
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2024-04-05 16:35:24 -0700

    Time: 1 ms (100.00%), Space: 42 MB (76.91%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,34 +1,30 @@
 class Solution {
     public String makeGood(String s) {     
-        if (s.length() == 0 || s.length() == 1) {
-            return s;            
+        if (s.length() < 2) {
+            return s;
         }
-
         StringBuilder sb = new StringBuilder(s);
-
         int i = 0;
-
-        
-        while (i < sb.length() - 1)
-        {
-            char leftChar = sb.charAt(i);
-            char rightChar = sb.charAt(i + 1);
-
-            if (Math.abs(leftChar - rightChar) == 32)
-            {
-                sb.delete(i, i + 2);
+        while (i < sb.length() - 1) {
+            char cur = sb.charAt(i);
+            char next = sb.charAt(i + 1);
+            if (Math.abs(next - cur) == 32) {
+                // sb.deleteCharAt(i);
+                // sb.deleteCharAt(i);
+                sb.delete(i, i + 2);   // the TC of StringBuilder delete operation is O(n)
                 if (i > 0) {
                     i--;
-                }      
+                }
             } else {
                 i++;
-            } 
+            }
         }
-
         return sb.toString();
     }
 }
 
-/*
 
+/*
+TC: worst case - O(n^2)
+SC: O(n)
 */
