69858d0a8da48e6a91e0aa77c642152ae1f0d664
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2024-04-12 11:44:45 -0700

    Time: 1 ms (99.79%), Space: 43 MB (95.72%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        Arrays.sort(nums);
+        List<Integer> cur = new ArrayList<>();
+        helper(nums, 0, cur, res);
+        return res;
+    }
+    
+    private void helper(int[] nums, int idx, List<Integer> cur, List<List<Integer>> res) {
+        if (idx == nums.length) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        
+        cur.add(nums[idx]);
+        helper(nums, idx + 1, cur, res);
+        cur.remove(cur.size() - 1);
+        
+        while (idx < nums.length - 1 && nums[idx + 1] == nums[idx]) {
+            idx++;
+        }
+        
+        helper(nums, idx + 1, cur, res);
+    }
+}
+
+/*
+[a,b,b]
+
+
+Recursion Tree
+									                              [		   ]
+            					                   /add				                  \ Not add
+Level 1:	                                      [a]                                   [ ]
+                                             /           \                           /           \
+Level 2:                            [a, b1]               [a]                   [b1]               [ ]
+                               /             \         /       \             /       \           /      \
+Level 3:                  [a, b1, b2]     [a, b1]  [a, b2]     [a]      [b1, b2]    [b1]      [b2]      [ ]
+                                          -------  -------                          ----      ----
+                                                  duplicate                                  duplicate
+                                                   pruning                                    pruning
+
+
+[[a, b, b], [a, b], [a], [b, b], [b], []]
+
+
+
+*/
