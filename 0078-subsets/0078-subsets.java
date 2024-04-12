0ade9899f525ea978c97013ae62decd7bb0e216e
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2024-04-12 10:24:16 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (93.50%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,27 +1,26 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return res;
-        }
-        
-        List<Integer> cur = new ArrayList<>();
-        helper(nums, 0, cur, res);
-        return res;
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (nums == null || nums.length == 0) {
+      return res;
+    }
+    List<Integer> cur = new ArrayList<>();
+    helper(nums, 0, cur, res);
+    return res;
+  }
+
+  private void helper(int[] nums, int idx, List<Integer> cur, List<List<Integer>> res) {
+    if (idx == nums.length) {
+      res.add(new ArrayList<>(cur));
+      return;
     }
     
-    private void helper(int[] nums, int idx, List<Integer> curList, List<List<Integer>> res) {  
-        if (idx == nums.length) {
-            res.add(new ArrayList<>(curList));
-            return;
-        }
-        
-        curList.add(nums[idx]);
-        helper(nums, idx + 1, curList, res);
-        curList.remove(curList.size() - 1);
-        
-        helper(nums, idx + 1, curList, res);
-    }
+    cur.add(nums[idx]);
+    helper(nums, idx + 1, cur, res);
+    cur.remove(cur.size() - 1);
+
+    helper(nums, idx + 1, cur, res);
+  }
 }
 
 /*
