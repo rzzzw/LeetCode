9466505ad28f83f7c006aeca23b8fa7754578cd3
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-09-25 16:28:11 -0700

    Time: 6 ms (99.2%), Space: 42.9 MB (51.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,22 +1,49 @@
+// class Solution {
+//     public String longestPalindrome(String s) {
+//         if (s == null || s.length() <= 1) return s;
+//         int n = s.length();
+//         char[] arr = s.toCharArray();
+//         int maxLen = 1;
+//         int head = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             int len = Math.max(getPalinLen(arr, i, i), getPalinLen(arr, i, i + 1));
+//             if (len > maxLen) {
+//                 maxLen = len;
+//                 head = i - (len - 1) / 2;
+//             }
+//         }
+//         return new String(arr, head, maxLen);
+//     }
+//     private int getPalinLen(char[] arr, int l, int r) {
+//         while(l >= 0 && r < arr.length && arr[l] == arr[r]) {
+//             l--;
+//             r++;
+//         }
+//         return r - l - 1;
+//     }
+// }
+
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() <= 1) return s;
-        int n = s.length();
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
         char[] arr = s.toCharArray();
-        int maxLen = 1;
         int head = 0;
-
-        for (int i = 0; i < n; i++) {
-            int len = Math.max(getPalinLen(arr, i, i), getPalinLen(arr, i, i + 1));
+        int maxLen = 1;
+        for (int fast = 0; fast < arr.length; fast++){
+            int len = Math.max(getPalinLen(arr, fast, fast), getPalinLen(arr, fast, fast+1));
             if (len > maxLen) {
                 maxLen = len;
-                head = i - (len - 1) / 2;
+                head = fast - (maxLen - 1)/2;
             }
         }
         return new String(arr, head, maxLen);
     }
+
     private int getPalinLen(char[] arr, int l, int r) {
-        while(l >= 0 && r < arr.length && arr[l] == arr[r]) {
+        while(l >= 0 && r <arr.length && arr[l] == arr[r]) {
             l--;
             r++;
         }
