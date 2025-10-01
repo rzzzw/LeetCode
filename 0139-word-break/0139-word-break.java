64ad16f626e1415003f5036a1235d95d5ce83b0b
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-10-01 16:06:13 -0700

    Time: 8 ms (42.51%), Space: 44 MB (59.83%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,48 @@
+/**
+        applepenapple       ["apple","pen"]
+
+M[i] represent 长度为i的string能不能切成每一份都在字典里
+M[0] = T
+
+len = 1 -- 'a' : M[1] = F
+len = 2 -- 'ap':
+    0': 
+        |ap
+        M[0] && 'ap'is not in the dict - F
+    1':
+        a|p
+        M[1] && "p" is not in the dict - F
+    M[2] = (F || F) = F
+len = 3 -- 'app':
+    0': |app => M[0] && 'app'is not in the dict - F
+    1': a|pp => M[1] && 'pp'is not in the dict - F
+    2': ap|p => M[2] && 'p'is not in the dict - F     
+    M[3] = (F||F||F) = F 
+
+
+ */
+
+ class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = toSet(wordDict);
+        boolean[] M = new boolean[s.length() + 1];
+        M[0] = true;
+        for (int len = 1; len <= s.length(); len++) {
+            for (int i = 0; i < len; i++) {
+                if (M[i] && set.contains(s.substring(i, len))) {
+                    M[len] = true;
+                    continue;
+                }
+            }
+        }
+        return M[M.length - 1];
+    }
+
+    private Set<String> toSet(List<String> list) {
+        Set<String> set = new HashSet<>();
+        for (String s : list) {
+            set.add(s);
+        }
+        return set;
+    }
+ }
