79a70aca091cb84ed555fe6033fc27038ffca780
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-10-03 10:54:34 -0700

    Time: 30 ms (71.25%), Space: 51.8 MB (35.02%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,16 +1,65 @@
-class Solution {
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if (nums == null || nums.length < 3) {
+//             return res;
+//         }
+
+//         Arrays.sort(nums);
+//         for (int i = 0; i < nums.length - 2; i++) {
+//             if (i > 0 && nums[i] == nums[i - 1]) {
+//                 continue;
+//             }
+
+//             int j = i + 1;
+//             int k = nums.length - 1;
+//             while (j < k) {
+//                 int sum = nums[i] + nums[j] + nums[k];
+//                 if (sum == 0) {
+//                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+//                     j++;
+//                     k--;
+//                     while (j < k && nums[j] == nums[j - 1]) {
+//                         j++;
+//                     }
+//                     while (j < k && nums[k] == nums[k + 1]) {
+//                         k--;
+//                     }
+//                 } else if (sum < 0) {
+//                     j++;
+//                 } else {
+//                     k--;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+/* time complexity
+
+        1  2  3  4  5  6  .....
+        ^  
+       n-1
+           ^
+          n-2
+              .........
+              
+   (n-1) + (n-2) +...+ 1 = (1+n-1)(n-1)/2 => n^2
+            
+*/
+
+class Solution{
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if (nums == null || nums.length < 3) {
             return res;
         }
-
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-
             int j = i + 1;
             int k = nums.length - 1;
             while (j < k) {
@@ -25,26 +74,14 @@ class Solution {
                     while (j < k && nums[k] == nums[k + 1]) {
                         k--;
                     }
-                } else if (sum < 0) {
-                    j++;
-                } else {
+                } else if (sum > 0) {
                     k--;
+                } else {
+                    j++;
                 }
             }
+            
         }
         return res;
     }
 }
-
-/* time complexity
-
-        1  2  3  4  5  6  .....
-        ^  
-       n-1
-           ^
-          n-2
-              .........
-              
-   (n-1) + (n-2) +...+ 1 = (1+n-1)(n-1)/2 => n^2
-            
-*/
