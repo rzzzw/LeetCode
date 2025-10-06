76e4fa4aa5402dbe2ef265d5cc06e791a17ae670
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-10-06 15:05:34 -0700

    Time: 3 ms (99.41%), Space: 46 MB (13.84%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -2,32 +2,28 @@ public class Codec {
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        StringBuilder encodedString = new StringBuilder();
+        StringBuilder encoded = new StringBuilder();
         for (String s : strs) {
-            encodedString.append(s.replace("/", "//")).append("/:");
+            encoded.append(s.length()).append('#').append(s);
         }
-        return encodedString.toString();
+        return encoded.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        List<String> decodedStrings = new ArrayList<>();
-        StringBuilder curStr = new StringBuilder();
+        List<String> result = new ArrayList<>();
         int i = 0;
         while (i < s.length()) {
-            if (i + 1 < s.length() && s.charAt(i) == '/' && s.charAt(i + 1) == ':') {
-                decodedStrings.add(curStr.toString());
-                curStr = new StringBuilder();
-                i += 2;
-            } else if (i + 1 < s.length() && s.charAt(i) == '/' && s.charAt(i + 1) == '/') {
-                curStr.append('/');
-                i += 2;
-            } else {
-                curStr.append(s.charAt(i));
-                i++;
+            int j = i;
+            while (s.charAt(j) != '#') {
+                j++;
             }
+            int length = Integer.parseInt(s.substring(i, j));
+            j++; // skip '#'
+            result.add(s.substring(j, j + length));
+            i = j + length;
         }
-        return decodedStrings;
+        return result;
     }
 }
 
