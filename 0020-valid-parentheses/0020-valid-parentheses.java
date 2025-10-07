8561ebdaff1ea2418e74b16e380359b97b651f8f
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-10-07 07:37:29 -0700

    Time: 4 ms (22.22%), Space: 41.9 MB (67.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,50 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         if (s == null || s.length() == 0) {
+//             return true;
+//         }
+//         Deque<Character> stack = new ArrayDeque<>();
+//         for (char c : s.toCharArray()) {
+//             if (c == '(') {
+//                 stack.offerFirst(')');
+//             } else if (c == '[') {
+//                 stack.offerFirst(']');
+//             } else if (c == '{') {
+//                 stack.offerFirst('}');
+//             } else {
+//                 if (stack.isEmpty()) {
+//                     return false;
+//                 } 
+//                 else if (stack.pollFirst() != c) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+
 class Solution {
     public boolean isValid(String s) {
         if (s == null || s.length() == 0) {
             return true;
         }
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
         Deque<Character> stack = new ArrayDeque<>();
         for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.offerFirst(')');
-            } else if (c == '[') {
-                stack.offerFirst(']');
-            } else if (c == '{') {
-                stack.offerFirst('}');
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                } 
-                else if (stack.pollFirst() != c) {
+            if (map.containsKey(c)) {
+                if (stack.isEmpty() || stack.peekFirst() != map.get(c)) {
                     return false;
                 }
+                stack.pollFirst();
+            } else {
+                stack.offerFirst(c);
             }
-        }
-        return stack.isEmpty();
+        }  
+        return stack.isEmpty();      
     }
 }
-
