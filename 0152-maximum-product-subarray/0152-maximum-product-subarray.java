4876ed13c0c38d1d2ccf57579477d56ec8d45b68
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-10-07 10:53:00 -0700

    Time: 2 ms (55.22%), Space: 44.8 MB (87.92%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,35 +1,16 @@
-// class Solution {
-//     public int maxProduct(int[] nums) {
-//         if (nums == null || nums.length == 0) return 0;
-//         int max = nums[0];
-//         int min = nums[0];
-//         int res = nums[0];
-
-//         for (int i = 1; i < nums.length; i++) {
-//             int maxPre = max;
-//             max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
-//             min = Math.min(Math.min(maxPre * nums[i], min * nums[i]), nums[i]);  // 注意此时 max 已经更新，用tmp
-//             res = Math.max(max, res);
-//         }
-//         return res;
-//     } 
-// }
-
 class Solution {
     public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        if (nums == null || nums.length == 0) return 0;
         int max = nums[0];
         int min = nums[0];
         int res = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
             int maxPre = max;
-            max = Math.max(nums[i], Math.max(nums[i] * max, nums[i] * min));
-            min = Math.min(nums[i], Math.min(nums[i] * maxPre, nums[i] * min));
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(maxPre * nums[i], min * nums[i]), nums[i]);  // 注意此时 max 已经更新，用tmp
             res = Math.max(max, res);
         }
         return res;
-    }
+    } 
 }
