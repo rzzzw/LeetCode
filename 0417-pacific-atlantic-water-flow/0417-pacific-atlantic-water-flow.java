c093b5372095baaa74de48847f1a44e0a31f9827
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-10-14 13:21:01 -0700

    Time: 4 ms (98.11%), Space: 45 MB (99.68%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,70 +1,55 @@
 class Solution {
-    private static final int[][] DIRS = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-    private int numRows;
-    private int numCols;
-    private int[][] heightsOri;
+    private static final int[][] DIRECTIONS = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private int nCols;
+    private int nRows;
+    private int[][] height;
 
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        // Check if input is empty
-        if (heights.length == 0 || heights[0].length == 0) {
+    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
             return new ArrayList<>();
         }
-
-        // Save initial values to parameters
-        numRows = heights.length;
-        numCols = heights[0].length;
-        heightsOri = heights;
-
-        // Setup each queue with cells adjacent to their respective ocean
-        // any problem with repeat cell?
-        Queue<int[]> pacificQ = new LinkedList<>();
-        Queue<int[]> atlanticQ = new LinkedList<>();
-        for (int i = 0; i < numRows; i++) {
-            pacificQ.offer(new int[]{i, 0});
-            atlanticQ.offer(new int[]{i, numCols - 1});
+        nRows = matrix.length;
+        nCols = matrix[0].length;
+        height = matrix;
+        boolean[][] pacificReachable = new boolean[nRows][nCols];
+        boolean[][] atlanticReachable = new boolean[nRows][nCols];
+        
+        // Loop through each cell adjacent to the oceans and start a DFS
+        for (int i = 0; i < nRows; i++) {
+            dfs(i, 0, pacificReachable);
+            dfs(i, nCols - 1, atlanticReachable);
         }
-        for (int i = 0; i < numCols; i++) {
-            pacificQ.offer(new int[]{0, i});
-            atlanticQ.offer(new int[]{numRows - 1, i});            
+        for (int i = 0; i < nCols; i++) {
+            dfs(0, i, pacificReachable);
+            dfs(nRows - 1, i, atlanticReachable);
         }
-        // Perform a BFS for each ocean to find all cells accessible by each ocean
-        boolean[][] reachPacific = bfs(pacificQ);
-        boolean[][] reachAtlantic = bfs(atlanticQ);
 
-        // Find all cells that can reach both Oceans
-        List<List<Integer>> commonGrids = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                if (reachPacific[i][j] && reachAtlantic[i][j]) {
-                    commonGrids.add(List.of(i,j));
+        List<List<Integer>> commonCells = new ArrayList<>();
+        for (int i = 0; i < nRows; i++) {
+            for (int j = 0; j < nCols; j++) {
+                if (pacificReachable[i][j] && atlanticReachable[i][j]) {
+                    commonCells.add(List.of(i, j));
                 }
             }
         }
-        return commonGrids;
+        return commonCells;
     }
 
-    private boolean[][] bfs(Queue<int[]> queue) {
-        boolean[][] reachable = new boolean[numRows][numCols];
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            // this cell is reachable, so mark it
-            reachable[cell[0]][cell[1]] = true;
-            for (int[] dir : DIRS) {
-                int newR = cell[0] + dir[0];
-                int newC = cell[1] + dir[1];
-
-                // check if new grid is in-bound
-                if (newR < 0 || newR >= numRows || newC < 0 || newC >= numCols) continue;
-
-                // Make sure that the new grid hasn't already been visited
-                if (reachable[newR][newC]) continue;
-
-                // Check that the new grid is >= current height.
-                if (heightsOri[newR][newC] < heightsOri[cell[0]][cell[1]]) continue;
-
-                queue.offer(new int[]{newR, newC});
+    private void dfs(int row, int col, boolean[][] reachable) {
+        reachable[row][col] = true;
+        for (int[] dir : DIRECTIONS) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            if (newRow < 0 || newRow >= nRows || newCol < 0 || newCol >= nCols) {
+                continue;
+            }  
+            if (reachable[newRow][newCol]) {
+                continue;
             }
+            if (height[newRow][newCol] < height[row][col]) {
+                continue;
+            }
+            dfs(newRow, newCol, reachable);
         }
-        return reachable;
     }
 }
