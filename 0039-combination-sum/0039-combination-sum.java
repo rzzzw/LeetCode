6f0e77c1d697b44ea8ca64205f2449a9267a5022
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-10-17 17:14:23 -0700

    Time: 2 ms (84.59%), Space: 45 MB (20.26%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -4,28 +4,58 @@ class Solution {
         if (candidates == null || candidates.length == 0) {
             return res;
         }
-        helper(res, new ArrayList<>(), 0, target, candidates);
+        helper(new ArrayList<>(), 0, res, candidates, target);
         return res;
     }
-    private void helper(List<List<Integer>> res, List<Integer> cur, int idx, int remains, int[] candidates) {
+    private void helper(List<Integer> cur, int idx, List<List<Integer>> res, int[] candidates, int remains) {
 
         if (remains == 0) {
             res.add(new ArrayList<>(cur));
             return;   
         }  
 
-        if (remains < 0 || idx == candidates.length) {
+        if (remains < 0) {
             return;
         }
 
-        cur.add(candidates[idx]);
-        helper(res, cur, idx, remains - candidates[idx], candidates);
-        cur.remove(cur.size() - 1);
-
-
-        helper(res, cur, idx + 1, remains, candidates);
+        for (int i = idx; i < candidates.length; i++) {
+            cur.add(candidates[i]);                
+            helper(cur, i, res, candidates, remains - candidates[i]);
+            cur.remove(cur.size() - 1);
+        }
     }
 }
+
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if (candidates == null || candidates.length == 0) {
+//             return res;
+//         }
+//         helper(res, new ArrayList<>(), candidates, target, 0);
+//         return res;
+//     }
+//     private void helper(List<List<Integer>> res, List<Integer> cur, int[] candidates, int remains, int idx) {
+//         if (remains == 0) {
+//             res.add(new ArrayList<>(cur));
+//             return;
+//         }
+
+//         if (idx == candidates.length) return;
+//         int val = candidates[idx];
+//         int maxCount = remains / val;
+
+//         for (int count = 0; count <= maxCount; count++) {
+//             helper(res, cur, candidates, remains - count * val, idx + 1);
+//             cur.add(val);
+//         }
+
+//         for (int k = 0; k < maxCount; k++) {
+//             cur.remove(cur.size() -1);
+//         }
+//     }
+// }
+
 
 
 
