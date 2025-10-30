1ec067f9aacbcfbc7f5bdde84c26ce9c6e0e74ef
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-10-29 22:53:08 -0700

    Time: 6 ms (98.37%), Space: 44.6 MB (92.48%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,4 +1,4 @@
-class Solution {
+ class Solution {
     public int characterReplacement(String s, int k) {
         int len = s.length();
         if (len < 2) {
@@ -8,7 +8,8 @@ class Solution {
         int[] freq = new int[26];
         int maxCount = 0;
         int left = 0;
-        int right = 0;
+        int right = 0; 
+        //[left, right) 区间内最多替换k个字符可以得到只有一种字符的字串， 区间左闭右开，长度=right-left
         while (right < len) {
             freq[arr[right] - 'A']++;
             maxCount = Math.max(maxCount, freq[arr[right] - 'A']);
