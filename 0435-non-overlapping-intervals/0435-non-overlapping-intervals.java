c1f01429ad3d72fb8092e2ddc8d69ff5df9bd265
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-03 13:36:39 -0800

    Time: 47 ms (69.67%), Space: 115.9 MB (5.02%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -3,20 +3,17 @@ class Solution {
         if (intervals.length == 0) {
             return 0;
         }
-        // Arrays.sort(intervals, Comparator.comparingInt(a -> a[1])); 
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
-        int res = 0;
-        int k = Integer.MIN_VALUE;
-        for (int i = 0; i < intervals.length; i++) {
-            int x = intervals[i][0];
-            int y = intervals[i][1];
-            if (x >= k) {
-                k = y;
+        int count = 0;
+        int prevEnd = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < prevEnd) {
+                count++;
             } else {
-                res++;
+                prevEnd = intervals[i][1];
             }
         }
-        return res;
+        return count;
     }
 }
 
@@ -28,4 +25,10 @@ class Solution {
         ________
                 ____
 
+
+        1   2   3   4   5   
+        ____
+        ________        
+            ____
+                ____
  */
