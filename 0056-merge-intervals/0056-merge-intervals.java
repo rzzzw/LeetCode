ad972670a4f8a462eaf3e3881a01f6a5c5c3364e
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-03 21:34:44 -0800

    Time: 2 ms (99.22%), Space: 41.7 MB (100%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,32 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length == 1) {
+        if (intervals == null || intervals.length < 2) {
             return intervals;
         }
-        int[] start = new int[intervals.length];
-        int[] end = new int[intervals.length];
-        for (int i = 0; i < intervals.length; i++) {
-            start[i] = intervals[i][0];
-            end[i] = intervals[i][1];
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for (int i = 0; i < starts.length; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
         }
-        Arrays.sort(start);
-        Arrays.sort(end);
-        List<int[]> res = new ArrayList<>();
-        int curStart = 0;
-        for (int i = 0; i < start.length; i++) {
-            if (i == start.length - 1 || start[i + 1] > end[i]) {
-                res.add(new int[]{start[curStart], end[i]});
-                curStart = i + 1;
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        List<int[]> list = new ArrayList<>();
+        int indexS = 0;
+        for (int i = 0; i < starts.length; i++) {
+            if (i == ends.length - 1 || starts[i + 1] > ends[i]) {
+                list.add(new int[] {starts[indexS], ends[i]});
+                indexS = i + 1;
             }
         }
-        return res.toArray(new int[res.size()][]);
+        return list.toArray(new int[list.size()][2]);
     }
 }
-
-
-/**
-            i
-start   1   2   8   15
-end     3   6   10  18
-
- */
