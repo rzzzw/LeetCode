99f7383957abc560d6f36cda6d5449d686ab04fe
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-03 21:21:28 -0800

    Time: 8 ms (85.21%), Space: 49.2 MB (5.09%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,58 @@
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+//         if (intervals == null || intervals.length == 1) {
+//             return intervals;
+//         } 
+//         int[] start = new int[intervals.length];
+//         int[] end = new int[intervals.length];
+//         for (int i = 0; i < intervals.length; i++) {
+//             start[i] = intervals[i][0];
+//             end[i] = intervals[i][1];
+//         }
+//         Arrays.sort(start);
+//         Arrays.sort(end);
+
+//         List<int[]> res = new ArrayList<>();
+//         int curStart = 0;
+//         for (int i = 0; i < start.length; i++) {
+//             if (i == start.length - 1 || start[i + 1] > end[i]) {
+//                 res.add(new int[]{start[curStart], end[i]});
+//                 curStart = i + 1;
+//             }
+//         }
+//         return res.toArray(new int[res.size() - 1][]);
+//     }
+// }
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 1) {
+            return intervals;
+        }
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        List<int[]> res = new ArrayList<>();
+        int curStart = 0;
+        for (int i = 0; i < start.length; i++) {
+            if (i == start.length - 1 || start[i + 1] > end[i]) {
+                res.add(new int[]{start[curStart], end[i]});
+                curStart = i + 1;
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
+
+
+/**
+            i
+start   1   2   8   15
+end     3   6   10  18
+
+ */
