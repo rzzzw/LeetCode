9e4390818440c27c4f6ecefc085a5cfdaa9d9770
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-03 21:23:19 -0800

    Time: 7 ms (98.24%), Space: 48.9 MB (5.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,29 +1,3 @@
-// class Solution {
-//     public int[][] merge(int[][] intervals) {
-//         if (intervals == null || intervals.length == 1) {
-//             return intervals;
-//         } 
-//         int[] start = new int[intervals.length];
-//         int[] end = new int[intervals.length];
-//         for (int i = 0; i < intervals.length; i++) {
-//             start[i] = intervals[i][0];
-//             end[i] = intervals[i][1];
-//         }
-//         Arrays.sort(start);
-//         Arrays.sort(end);
-
-//         List<int[]> res = new ArrayList<>();
-//         int curStart = 0;
-//         for (int i = 0; i < start.length; i++) {
-//             if (i == start.length - 1 || start[i + 1] > end[i]) {
-//                 res.add(new int[]{start[curStart], end[i]});
-//                 curStart = i + 1;
-//             }
-//         }
-//         return res.toArray(new int[res.size() - 1][]);
-//     }
-// }
-
 class Solution {
     public int[][] merge(int[][] intervals) {
         if (intervals == null || intervals.length == 1) {
@@ -45,7 +19,7 @@ class Solution {
                 curStart = i + 1;
             }
         }
-        return res.toArray(new int[res.size() - 1][]);
+        return res.toArray(new int[res.size()][]);
     }
 }
 
