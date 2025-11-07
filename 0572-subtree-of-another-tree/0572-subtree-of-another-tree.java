5ecb62967bb7fe4810ef874fedc85179e5fe6a26
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-07 09:39:44 -0800

    Time: 2 ms (98.15%), Space: 46.2 MB (7.37%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,26 +15,16 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null) {
-            return false;
-        }
-        if (isIdentical(root, subRoot)) {
-            return true;
-        }
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        if(root == null || subRoot == null) return false;
+        if(isSame(root,subRoot) == true) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot); 
     }
 
-    private boolean isIdentical(TreeNode a, TreeNode b) {
-        if (a == null && b == null) {
-            return true;
-        }
-        if (a == null || b == null) {
-            return false;
-        }
-        if (a.val != b.val) {
-            return false;
-        }
-
-        return (isIdentical(a.left, b.left) && isIdentical(a.right, b.right));
+    private boolean isSame(TreeNode a, TreeNode b)
+    {
+        if(a == null && b == null) return true;
+        if(a == null || b == null) return false;
+        if(a.val != b.val) return false;
+        return isSame(a.left, b.left) && isSame(a.right, b.right);
     }
 }
