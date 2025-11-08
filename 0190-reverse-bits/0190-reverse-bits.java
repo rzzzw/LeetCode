f5341ee3d0fe7c3fd57f97633341f78d46395d82
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-08 08:07:53 -0800

    Time: 0 ms (100%), Space: 42.5 MB (6.73%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -2,8 +2,7 @@ class Solution {
     public int reverseBits(int n) {
         int res = 0;
         for (int i = 0; i < 32; i++) {
-            res <<= 1;
-            res |= (n & 1);
+            res = (res << 1) | (n & 1);
             n >>>= 1;
         }
         return res;
