8fe0325fef862f2434c245ce954a43fd214dc5c8
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-08 08:10:25 -0800

    Time: 1 ms (26.14%), Space: 42.3 MB (8.67%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -8,3 +8,28 @@ class Solution {
         return res;
     }
 }
+
+/**
+n = 1101   (we want to reverse → 1011)
+result = 0
+
+Step 1:
+  result = (0 << 1) | (1101 & 1)
+          = 0 | 1 = 1
+  n >>= 1 → 110
+
+Step 2:
+  result = (1 << 1) | (110 & 1)
+          = (10) | 0 = 10
+  n >>= 1 → 11
+
+Step 3:
+  result = (10 << 1) | (11 & 1)
+          = (100) | 1 = 101
+  n >>= 1 → 1
+
+Step 4:
+  result = (101 << 1) | (1 & 1)
+          = (1010) | 1 = 1011
+
+ */
