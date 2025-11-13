3a716c1dba552e6bb1f7673c69ae4b46e8878e81
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-12 23:07:57 -0800

    Time: 1 ms (100%), Space: 46.4 MB (15.54%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,28 +1,28 @@
 // union-find
-class Solution{
-    public int countComponents(int n, int[][] edges) {
-        int[] parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-        int count = n;
-        for (int[] edge : edges){
-            int rootA = find(parent, edge[0]);
-            int rootB = find(parent, edge[1]);
-            if (rootA != rootB) {
-                parent[rootA] = rootB;
-                count--;
-            }
-        }
-        return count;
-    } 
-    private int find(int[] parent, int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent, parent[x]);
-        }
-        return parent[x];
-    } 
-}
+// class Solution{
+//     public int countComponents(int n, int[][] edges) {
+//         int[] parent = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             parent[i] = i;
+//         }
+//         int count = n;
+//         for (int[] edge : edges){
+//             int rootA = find(parent, edge[0]);
+//             int rootB = find(parent, edge[1]);
+//             if (rootA != rootB) {
+//                 parent[rootA] = rootB;
+//                 count--;
+//             }
+//         }
+//         return count;
+//     } 
+//     private int find(int[] parent, int x) {
+//         if (parent[x] != x) {
+//             parent[x] = find(parent, parent[x]);
+//         }
+//         return parent[x];
+//     } 
+// }
 
 
 // dfs
@@ -55,3 +55,45 @@ class Solution{
 //         }
 //     }
 // }
+
+
+// union-find with rank
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] parent = new int[n];
+        int[] rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        int components = n;
+        for (int[] e : edges) {
+            if (union(e[0], e[1], parent, rank)) {
+                components--;
+            }
+        }
+        return components;
+    }
+    private boolean union(int a, int b, int[] parent, int[] rank) {
+        int rootA = find(a, parent);
+        int rootB = find(b, parent);
+        if (rootA == rootB) { // [0,1][0,2][1,2]
+            return false;
+        }
+        if (rank[rootA] < rank[rootB]) {
+            parent[rootA] = rootB;
+        } else if (rank[rootA] > rank[rootB]) {
+            parent[rootB] = rootA;
+        } else {
+            parent[rootB] = rootA;
+            rank[rootA]++;
+        }
+        return true;
+    }
+    private int find(int x, int[] parent) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x], parent);
+        }
+        return parent[x];
+    }
+}
