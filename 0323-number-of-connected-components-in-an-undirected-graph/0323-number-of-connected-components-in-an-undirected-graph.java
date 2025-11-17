8b4b960247a32bd957ef60261c0211da93b058c4
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-17 15:24:13 -0800

    Time: 2 ms (73.58%), Space: 46 MB (33.54%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -66,6 +66,78 @@
 //         return parent[x];
 //     } 
 // }
+/**
+Example 1:
+Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+Output: 2
+
+parent:
+    0 1 2 3 4
+    0 1 2 3 4
+count: 5
+
+[0, 1]
+    rootA = find(parent, 0)
+        parent[0] = 0 => rootA = 0 
+    rootB = find(parent, 1)
+        parent[1] = 1 => rootB = 1
+    rootA != rootB
+        parent[rootA] = rootB
+        parent[0] = 1
+    parent:
+        0 1 2 3 4
+        1 1 2 3 4
+    count: 4
+[1, 2]
+    rootA = find(parent, 1)
+        parent[1] = 1 => rootA = 1 
+    rootB = find(parent, 2)
+        parent[2] = 2 => rootB = 2
+    rootA != rootB
+        parent[rootA] = rootB
+        parent[1] = 2
+    parent:
+        0 1 2 3 4
+        1 2 2 3 4
+    count: 3
+
+[3, 4]
+    rootA = find(parent, 3)
+        parent[3] = 3 => rootA = 3 
+    rootB = find(parent, 4)
+        parent[4] = 4 => rootB = 4
+    rootA != rootB
+        parent[rootA] = rootB
+        parent[3] = 4
+    parent:
+        0 1 2 3 4
+        1 2 2 4 4
+    count: 2
+ */
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        int count = n;
+        for (int[] e : edges) {
+            int rootA = find(parent, e[0]);
+            int rootB = find(parent, e[1]);
+            if (rootA != rootB) {
+                parent[rootA] = rootB;
+                count--; 
+            }
+        }
+        return count;
+    }
+    private int find(int[] parent, int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent, parent[x]);
+        }
+        return parent[x];
+    }
+}
 
 
 // dfs
@@ -97,36 +169,36 @@ Complexity:
 
 */
 
-class Solution {
-    public int countComponents(int n, int[][] edges) {
-        if (n == 0 || edges == null || edges.length == 0) {
-            return 0;
-        }
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int[] e : edges) {
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
-        }
-        boolean[] visited = new boolean[n];
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(graph, visited, i);
-                count++;
-            }
-        }
-        return count;
-    }
-    private void dfs(List<List<Integer>> graph, boolean[] visited, int node) {
-        visited[node] = true;
-        for (int nei : graph.get(node)) {
-            if (!visited[nei]) {
-                dfs(graph, visited, nei);
-            }
-        }
-    }
-}
+// class Solution {
+//     public int countComponents(int n, int[][] edges) {
+//         if (n == 0 || edges == null || edges.length == 0) {
+//             return 0;
+//         }
+//         List<List<Integer>> graph = new ArrayList<>();
+//         for (int i = 0; i < n; i++) {
+//             graph.add(new ArrayList<>());
+//         }
+//         for (int[] e : edges) {
+//             graph.get(e[0]).add(e[1]);
+//             graph.get(e[1]).add(e[0]);
+//         }
+//         boolean[] visited = new boolean[n];
+//         int count = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 dfs(graph, visited, i);
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     private void dfs(List<List<Integer>> graph, boolean[] visited, int node) {
+//         visited[node] = true;
+//         for (int nei : graph.get(node)) {
+//             if (!visited[nei]) {
+//                 dfs(graph, visited, nei);
+//             }
+//         }
+//     }
+// }
 
