659d929f3a81c0a50b83014a48483d695fefc1c0
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-11-17 10:55:14 -0800

    Time: 5 ms (44.87%), Space: 46.7 MB (8.31%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -97,35 +97,6 @@ Complexity:
 
 */
 
-// class Solution {
-//     public int countComponents(int n, int[][] edges) {
-//         List<List<Integer>> graph = new ArrayList<>();
-//         for (int i = 0; i < n; i++) {
-//             graph.add(new ArrayList<>());
-//         }
-//         for (int[] e : edges) {
-//             graph.get(e[0]).add(e[1]);
-//             graph.get(e[1]).add(e[0]);
-//         }
-//         boolean[] visited = new boolean[n];
-//         int count = 0;
-//         for (int i = 0; i < n; i++) {
-//             if (!visited[i]) {
-//                 dfs(graph, visited, i);
-//                 count++;
-//             }
-//         }
-//         return count;
-//     }
-//     private void dfs(List<List<Integer>> graph, boolean[] visited, int node) {
-//         visited[node] = true;
-//         for (int neighbor : graph.get(node)) {
-//             if (!visited[neighbor]) {
-//                 dfs(graph, visited, neighbor);
-//             }
-//         }
-//     }
-// }
 class Solution {
     public int countComponents(int n, int[][] edges) {
         if (n == 0 || edges == null || edges.length == 0) {
