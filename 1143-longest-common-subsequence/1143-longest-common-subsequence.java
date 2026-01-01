1f361ef8f4798c0a00c43a846debb294470194c1
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2025-12-31 22:37:40 -0800

    Time: 20 ms (78.01%), Space: 53.9 MB (49.63%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,39 @@
+/**
+DP definition:
+dp[i][j] = length of LCS between:
+           text1[0..i-1] and text2[0..j-1]  (i and j represent lengths, not indices)
+Base case:
+          dp[0][*] = 0
+          dp[*][0] = 0
+
+Transition rules (core logic):
+    compare text1.charAt(i - 1) and text2.charAt(j - 1)
+    1' Characters match -> text1[i - 1] == text2[j - 1]
+        extend the LCS: dp[i][j] = dp[i - 1][j - 1] + 1
+    2' Characters do NOT match -> text1[i - 1] != text2[j - 1]
+        skip one character (try both possibilities):
+            dp[i][j] = max(
+                dp[i - 1][j],   // skip text1 char
+                dp[i][j - 1]    // skip text2 char
+            )
+
+ */
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m;  i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
