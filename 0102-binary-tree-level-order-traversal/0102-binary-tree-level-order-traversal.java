0d0a6a770c09706ea5ff19357f5996ae1bb15666
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-01 21:28:35 -0800

    Time: 2 ms (22.84%), Space: 47 MB (18.99%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,114 +13,92 @@
  *     }
  * }
  */
-class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null) {
-            return res;
-        }
-        helper(root, 0, res);
-        return res;
-    }
-    
-    private void helper(TreeNode node, int level, List<List<Integer>> res) {
-        if (level == res.size()) {
-            res.add(new ArrayList<Integer>());
-        }
-        res.get(level).add(node.val);
-        if (node.left != null) {
-            helper(node.left, level + 1, res);
-        }
-        if (node.right != null) {
-            helper(node.right, level + 1, res);
-        }
-    }
-}
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if (root == null) {
+//             return res;
+//         }
+//         helper(root, 0, res);
+//         return res;
+//     }
+
+//     public void helper(TreeNode node, int level, List<List<Integer>> res) {
+//         if (res.size() == level) {
+//             res.add(new ArrayList<Integer>());
+//         }
+//         res.get(level).add(node.val);
+//         if (node.left != null) {
+//             helper(node.left, level + 1, res);
+//         }
+//         if (node.right != null) {
+//             helper(node.right, level + 1, res);
+//         }
+//     }
+// }
+
+
 
 /*
-
-
-
-
-
-// BFS:
         1
       /   \
      2     3
     / \
    4   5
    
-   q:    4 5
-   size: 0
-   cur:  3
-   curL: {2, 3}
-   
-   {{1}, {2, 3}}
+[[1][2][4, 5]]
 
-TC: O(N)
-SC: O(N)
+*/
 
-class Solution {
+// BFS:
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if (root == null) return res;
+//         Queue<TreeNode> q = new ArrayDeque<>();
+//         q.offer(root);
+//         while (!q.isEmpty()) {
+//             List<Integer> curLayer = new ArrayList<>();
+//             int size = q.size();
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode cur = q.poll();
+//                 curLayer.add(cur.val);
+//                 if (cur.left != null) q.offer(cur.left);
+//                 if (cur.right != null) q.offer(cur.right);
+//             }
+//             res.add(curLayer);
+//         }
+//         return res;
+//     }
+// }
+
+class Solution{
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        if (root == null) return res;
+        if (root == null) {
+            return res;
+        }
         Queue<TreeNode> q = new ArrayDeque<>();
         q.offer(root);
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             List<Integer> curLayer = new ArrayList<>();
             int size = q.size();
-            for (int i = 0; i < size; i++) {
+            while (size > 0) {
                 TreeNode cur = q.poll();
                 curLayer.add(cur.val);
-                if (cur.left != null) q.offer(cur.left);
-                if (cur.right != null) q.offer(cur.right);
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+                size--;
             }
             res.add(curLayer);
         }
         return res;
     }
 }
-
-// DFS:
-
-            1
-          /   \
-         2     3
-        / \
-       4   5
-
-    helper(Node5, level2, res)
-    level:    2
-    res: {{1}, {2}, {4, 5}}
-
-TC: O(N)
-SC: O(N) <= O(N + height)
-
-class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null) return res;
-        helper(root, 0, res);
-        return res;
-    }
-    
-    private void helper(TreeNode node, int level, List<List<Integer>> res) {
-        if (res.size() == level) {
-            res.add(new ArrayList<Integer>());
-        }
-        res.get(level).add(node.val);
-        
-        if (node.left != null) {
-            helper(node.left, level + 1, res);
-        }
-        if (node.right != null) {
-            helper(node.right, level + 1, res);
-        }
-    }
-}
-
-
-*/
 
 
 
