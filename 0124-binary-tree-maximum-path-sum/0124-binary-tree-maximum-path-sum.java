b4e5a3dd566ec3faadce1857780f8200ba8c5b88
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-08 20:15:06 -0800

    Time: 0 ms (100%), Space: 46.6 MB (59.33%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -13,32 +13,51 @@
  *     }
  * }
  */
+// class Solution {
+//     public int maxPathSum(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+//         int[] max = new int[]{Integer.MIN_VALUE};
+//         helper(root, max);
+//         return max[0];
+
+//     }
+
+//     private int helper(TreeNode root, int[] max) {
+//         if (root == null) {
+//             return 0;
+//         }
+//         int left = helper(root.left, max);
+//         int right = helper(root.right, max);       
+//         max[0] = Math.max(max[0], left + right + root.val);
+
+//         // return left >= right ? (left + root.val) : (right + root.val);  ❌ 这种写法的结果是，通往两个叶子结点。但对于这道题，如果一个分支从某个中间节点开始往下是负数，则该中间节点及以下应该被抛弃
+//         return Math.max(0, Math.max(left, right) + root.val); 
+
+
+//     }
+// }
+
+/**
+ */
+
 class Solution {
     public int maxPathSum(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        int[] res = new int[]{Integer.MIN_VALUE};
-        helper(root, res);
-        return res[0];
+        int[] max = new int[]{Integer.MIN_VALUE};
+        helper(root, max);
+        return max[0];
     }
-    // return the maxSum of 1 path
-    private int helper(TreeNode root, int[] res) {
-        if (root == null) {
+    private int helper(TreeNode node, int[] max) {
+        if (node == null) {
             return 0;
         }
-        int left = helper(root.left, res);
-        int right = helper(root.right, res);
-        res[0] = Math.max(res[0], left + right + root.val); // result can be a negative number
-        
-        return Math.max(0, Math.max(left, right) + root.val);  // if the sum of a subtree from one node is a negative number, the subtree from that node need to be discarded
-        // return left >= right ? (left + root.val) : (right + root.val);  ❌ this is for leaf to leaf case.
+        int left = helper(node.left, max);
+        int right = helper(node.right, max);
+        max[0] = Math.max(max[0], left + right + node.val);
+        return Math.max(0, node.val + Math.max(left, right));
     }
 }
-
-/**
-              1
-            /   \
-          -1     2
-
- */
