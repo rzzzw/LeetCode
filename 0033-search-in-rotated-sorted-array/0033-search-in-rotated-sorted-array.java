e1e32185abad6551bb0463dcfef6b92502fedbd9
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-12 23:55:19 -0800

    Time: 0 ms (100%), Space: 43.9 MB (50.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,40 +1,14 @@
-// class Solution {
-//     public int search(int[] nums, int target) {
-//         int lo = 0;
-//         int hi = nums.length - 1;
-//         while (lo <= hi) {
-//             int mid = lo + (hi - lo) / 2;
-//             if (nums[mid] == target) {
-//                 return mid;
-//             } else if (nums[lo] <= nums[mid]) {
-//                 if (target < nums[mid] && target >= nums[lo]) {
-//                     hi = mid - 1;
-//                 } else {
-//                     lo = mid + 1;
-//                 }
-//             } else {
-//                 if (target > nums[mid] && target <= nums[hi]) {
-//                     lo = mid + 1;
-//                 } else {
-//                     hi = mid - 1;
-//                 }
-//             }
-//         }
-//         return -1;
-//     }
-// }
-
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
+        int left = 0; 
         int right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[left] < nums[mid]) {
-                if (target < nums[mid] && target >= nums[left]) {
+            if (nums[left] <= nums[mid]) {
+                if (target < nums[mid] && target >= nums[left]){
                     right = mid - 1;
                 } else {
                     left = mid + 1;
@@ -45,7 +19,6 @@ class Solution {
                 } else {
                     right = mid - 1;
                 }
-
             }
         }
         return -1;
