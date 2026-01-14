cd85d15794cb24ddec1dd9234ce0e88401484fe3
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-13 22:17:43 -0800

    Time: 0 ms (100%), Space: 43.9 MB (40.78%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -38,27 +38,3 @@ Key observation (critical)：
         The right side always contains the smallest element when unsorted
 
 */
-
-// class Solution {
-//     public int findMin(int[] nums) {
-//         if (nums == null || nums.length == 0) {
-//             return 0;
-//         }        
-//         int left = 0;
-//         int right = nums.length - 1;
-
-//         if (nums[left] < nums[right]) return nums[left];
-
-//         while(left < right) {
-//             int mid = left + (right - left) / 2;
-
-//             if(nums[mid] < nums[right]) {
-//                 right = mid;
-//             } else {
-//                 left = mid + 1;
-//             }
-//         }
-
-//         return nums[left];
-//     }
-// }
