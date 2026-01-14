90500f7b0e8289ca35b5297a2a01705bea5a1d4a
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-13 23:06:18 -0800

    Time: 0 ms (100%), Space: 43 MB (74.24%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -16,48 +16,19 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        TreeNode nextVisit = root;
-        while (nextVisit != null || !stack.isEmpty()) {
-            if (nextVisit != null) {
-                stack.offerFirst(nextVisit);
-                nextVisit = nextVisit.left;
-            } else {
-                TreeNode cur = stack.pollFirst();
-                res.add(cur.val);
-                nextVisit = cur.right;
-            }
-            
+        if (root == null) {
+            return res;
         }
+        inOrder(root, res);
         return res;
     }
+    private void inOrder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        
+        inOrder(node.left, res);
+        res.add(node.val);
+        inOrder(node.right, res);
+    }
 }
-/*
-        1
-       / \ 
-      2   3
-
-res 2 1
-Deque: 
-nextVisit: node3
-cur: node1
-
-*/
-// Recursion:
-// class Solution {
-//     public List<Integer> inorderTraversal(TreeNode root) {
-//         List<Integer> res = new ArrayList<>();
-//         inorderTraversal(root, res);
-//         return res;
-//     }
-    
-//     private void inorderTraversal(TreeNode root, List<Integer> res) {
-//         if (root == null) {
-//             return;
-//         }
-//         inorderTraversal(root.left, res);
-//         res.add(root.val);
-//         inorderTraversal(root.right, res);
-//     }
-// }
