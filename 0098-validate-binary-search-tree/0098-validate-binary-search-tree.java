acd1b7d5c534ac20279b559f7f509ea2bca3aeba
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-14 09:22:37 -0800

    Time: 0 ms (100%), Space: 45.3 MB (26.7%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -40,19 +40,38 @@ Space: O(h) recursion stack
 
 
 //Inorder traversal (sorted array insight - Inorder traversal of a BST is strictly increasing)
+// class Solution {
+//     Long prev = null;
+//     public boolean isValidBST(TreeNode root) {
+//         if (root == null) {
+//             return true;
+//         }
+//         if (!isValidBST(root.left)) return false;
+
+//         if (prev != null && root.val <= prev) {
+//             return false;
+//         }
+//         prev = (long) root.val;
+
+//         return isValidBST(root.right);
+//     }
+// }
+
+
 class Solution {
-    Long prev = null;
     public boolean isValidBST(TreeNode root) {
         if (root == null) {
             return true;
         }
-        if (!isValidBST(root.left)) return false;
-
-        if (prev != null && root.val <= prev) {
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    private boolean dfs(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= min || node.val >= max) {
             return false;
         }
-        prev = (long) root.val;
-
-        return isValidBST(root.right);
+        return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
     }
 }
