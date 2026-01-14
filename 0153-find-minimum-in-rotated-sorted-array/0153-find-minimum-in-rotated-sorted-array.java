d450ea93923f2d900389adade50332e8e1f1441a
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-13 22:17:36 -0800

    Time: 0 ms (100%), Space: 43.9 MB (23.36%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,23 +1,64 @@
 class Solution {
     public int findMin(int[] nums) {
         if (nums == null || nums.length == 0) {
-            return 0;
-        }        
-        int left = 0;
+            return Integer.MIN_VALUE;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int left = 0; 
         int right = nums.length - 1;
-
-        if (nums[left] < nums[right]) return nums[left];
 
         while(left < right) {
             int mid = left + (right - left) / 2;
-
-            if(nums[mid] < nums[right]) {
-                right = mid;
-            } else {
+            if (nums[mid] > nums[right]) { // Minimum is in the right half
                 left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-
         return nums[left];
     }
+
 }
+/** 
+Identify which half is sorted. 
+
+Key observation (critical)：
+
+    At any point:
+        If nums[mid] > nums[right]
+        → Minimum is to the right of mid
+
+    Else
+        → Minimum is at mid or to the left
+
+    Why?
+        The rotation break causes the drop
+        The right side always contains the smallest element when unsorted
+
+*/
+
+// class Solution {
+//     public int findMin(int[] nums) {
+//         if (nums == null || nums.length == 0) {
+//             return 0;
+//         }        
+//         int left = 0;
+//         int right = nums.length - 1;
+
+//         if (nums[left] < nums[right]) return nums[left];
+
+//         while(left < right) {
+//             int mid = left + (right - left) / 2;
+
+//             if(nums[mid] < nums[right]) {
+//                 right = mid;
+//             } else {
+//                 left = mid + 1;
+//             }
+//         }
+
+//         return nums[left];
+//     }
+// }
