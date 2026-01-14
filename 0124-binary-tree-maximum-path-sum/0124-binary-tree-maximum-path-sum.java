576bcb071773357e826943a4d4c842bbcff56708
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-13 21:03:10 -0800

    Time: 1 ms (30.19%), Space: 46.9 MB (23.93%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -13,6 +13,13 @@
  *     }
  * }
  */
+
+
+/**
+Time: (n)
+Space: O(h)
+ */
+
 class Solution {
     public int maxPathSum(TreeNode root) {
         if (root == null) {
@@ -21,26 +28,14 @@ class Solution {
         int[] max = new int[]{Integer.MIN_VALUE};
         helper(root, max);
         return max[0];
-
     }
-
-    private int helper(TreeNode root, int[] max) {
-        if (root == null) {
+    private int helper(TreeNode node, int[] max) {  // why not "void"? what value need to be returned
+        if (node == null) {
             return 0;
         }
-        int left = helper(root.left, max);
-        int right = helper(root.right, max);       
-        max[0] = Math.max(max[0], left + right + root.val);
-
-        // return left >= right ? (left + root.val) : (right + root.val);  ❌ 这种写法的结果是，通往两个叶子结点。但对于这道题，如果一个分支从某个中间节点开始往下是负数，则该中间节点及以下应该被抛弃
-        return Math.max(0, Math.max(left, right) + root.val); 
-
-
+        int left = helper(node.left, max);
+        int right = helper(node.right, max);
+        max[0] = Math.max(max[0], left + right + node.val);
+        return Math.max(0, Math.max(left, right) + node.val);
     }
 }
-
-/**
-Time: (n)
-Space: O(h)
- */
-
