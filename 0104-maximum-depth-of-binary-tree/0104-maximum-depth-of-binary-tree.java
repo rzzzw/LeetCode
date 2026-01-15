b2adefe7e1c363bcfae23b445d380e4d1c1d9837
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-14 22:10:46 -0800

    Time: 0 ms (100%), Space: 44.3 MB (69.32%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -19,5 +19,32 @@ class Solution {
             return 0;
         }
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-    } 
+    }
 }
+
+// BFS
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+//         if (root == null) return 0;
+
+//         Queue<TreeNode> queue = new ArrayDeque<>();
+//         queue.offer(root);
+
+//         int depth = 0;
+
+//         while (!queue.isEmpty()) {
+//             int levelSize = queue.size(); // nodes at current level
+//             depth++;                      // entering a new level
+
+//             for (int i = 0; i < levelSize; i++) {
+//                 TreeNode node = queue.poll();
+//                 if (node.left != null) queue.offer(node.left);
+//                 if (node.right != null) queue.offer(node.right);
+//             }
+//         }
+
+//         return depth;
+//     }
+// }
+
+
