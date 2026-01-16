c18cb3f4f7ae17c1d00b6e11af2768a3c937de84
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-15 22:12:30 -0800

    Time: 0 ms (100%), Space: 43.1 MB (31.96%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -13,12 +13,12 @@
  *     }
  * }
  */
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
         }
-
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
