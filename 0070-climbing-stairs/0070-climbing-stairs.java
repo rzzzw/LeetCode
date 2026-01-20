1073e04e6ac1f8a2339ea08a5eb992a08ef5f831
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-20 11:46:06 -0800

    Time: 0 ms (100%), Space: 42.3 MB (23.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,5 +1,8 @@
-public class Solution {
+class Solution {
     public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
         if (n == 1) {
             return 1;
         }
@@ -8,7 +11,12 @@ public class Solution {
         dp[2] = 2;
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
-        } 
+        }
         return dp[n];
     }
 }
+
+/**
+DP definition/state: dp[i] = number of distinct ways to reach the top
+DP transition:       dp[i] = dp[i - 1] + dp[i - 2];
+ */
