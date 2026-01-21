a39647cc53d01cfd5683062536ad4500b1b3f291
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-20 23:09:45 -0800

    Time: 3 ms (19.29%), Space: 47.4 MB (22.44%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,16 +1,17 @@
-class Solution {
+class Solution{
     public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         int max = nums[0];
         int min = nums[0];
         int res = nums[0];
-
         for (int i = 1; i < nums.length; i++) {
-            int maxPre = max;
+            int preMax = max;
             max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
-            min = Math.min(Math.min(maxPre * nums[i], min * nums[i]), nums[i]);  // 注意此时 max 已经更新，用tmp
-            res = Math.max(max, res);
+            min = Math.min(Math.min(preMax * nums[i], min * nums[i]), nums[i]); // 注意此时 max 已经更新，用tmp
+            res = Math.max(res, max);
         }
         return res;
-    } 
+    }
 }
