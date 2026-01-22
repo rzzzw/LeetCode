775bf84f0b325ca146b9ed99a6a0eac406eb09b8
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-21 22:00:14 -0800

    Time: 38 ms (67.1%), Space: 46.2 MB (62.8%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,52 +1,62 @@
-// Method 2. Build a Subsequence, get size
+// // Method 2. Build a Subsequence, get size
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         if (nums == null) {
+//             return 0;
+//         }
+//         if (nums.length <= 1) {
+//             return nums.length;
+//         }
+//         List<Integer> sub = new ArrayList<>();
+//         sub.add(nums[0]);
+//         for (int i = 1; i < nums.length; i++) {
+//             int num = nums[i];
+//             if (num > sub.get(sub.size() - 1)) {
+//                 sub.add(num);
+//             } else {
+//                 int j = 0;
+//                 while (num > sub.get(j)) {
+//                     j++;
+//                 }
+//                 sub.set(j, num);
+//             }
+//         }
+//         return sub.size();
+//     }
+// }
+/**
+Method 1. DP
+dp states:  dp[i] = length of the longest increasing subsequence that ends at index i
+
+dp transition: 
+            dp[i] = 1 
+            for j in [0 .. i-1]:
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+         
+ */
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        if (nums == null) {
-            return 0;
-        }
-        if (nums.length <= 1) {
-            return nums.length;
-        }
-        List<Integer> sub = new ArrayList<>();
-        sub.add(nums[0]);
-        for (int i = 1; i < nums.length; i++) {
-            int num = nums[i];
-            if (num > sub.get(sub.size() - 1)) {
-                sub.add(num);
-            } else {
-                int j = 0;
-                while (num > sub.get(j)) {
-                    j++;
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int res = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
-                sub.set(j, num);
             }
+            res = Math.max(dp[i], res);
         }
-        return sub.size();
+        return res;
     }
 }
 
-// Method 1. DP
-// class Solution {
-//     public int lengthOfLIS(int[] nums) {
-//         int[] dp = new int[nums.length];
-//         Arrays.fill(dp, 1);
-        
-//         for (int i = 1; i < nums.length; i++) {
-//             for (int j = 0; j < i; j++) {
-//                 if (nums[i] > nums[j]) {
-//                     dp[i] = Math.max(dp[i], dp[j] + 1);
-//                 }
-//             }
-//         }
-        
-//         int longest = 0;
-//         for (int c: dp) {
-//             longest = Math.max(longest, c);
-//         }
-        
-//         return longest;
-//     }
-// }
+
 
 // class Solution {
 //     public int lengthOfLIS(int[] nums) {
