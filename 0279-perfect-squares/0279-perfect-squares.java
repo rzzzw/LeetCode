88dc691d4d8bbddb875f5a17535034b0e04dd123
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-21 17:13:36 -0800

    Time: 30 ms (75.96%), Space: 44.7 MB (36.23%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numSquares(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] M = new int[n + 1];
+        M[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            M[i] = i; //initialize w worst case, maximum i cuts - all are 1^2 
+                      //cautions: M[i] default as 0 if not assign any other value
+            for (int j = 1; j * j <= i; j++) {
+                M[i] = Math.min(M[i - j * j] + 1, M[i]);
+            }
+        }
+        return M[n];
+    }
+}
+
+// TC: n * sqt(n)
+// SC: n
+
+/**
+dp[i] = minimum number of squares to sum to i
+
+For each i, try all square numbers j*j ≤ i:
+    dp[i] = min(dp[i - j*j] + 1)
+
+Base case: dp[0] = 0
+
+ */
