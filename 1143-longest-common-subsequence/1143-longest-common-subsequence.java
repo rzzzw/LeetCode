29eb7ea3b02b915d0c5fdc4eef893280dde26e5b
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-22 16:08:56 -0800

    Time: 20 ms (77.94%), Space: 54.1 MB (42.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -19,36 +19,13 @@ Transition rules (core logic):
 
  */
 
-// class Solution {
-//     public int longestCommonSubsequence(String text1, String text2) {
-//         int m = text1.length();
-//         int n = text2.length();
 
-//         int[][] dp = new int[m + 1][n + 1];
-//         for (int i = 1; i <= m;  i++) {
-//             for (int j = 1; j <= n; j++) {
-//                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-//                     dp[i][j] = dp[i - 1][j - 1] + 1;
-//                 } else {
-//                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-//                 }
-//             }
-//         }
-//         return dp[m][n];
-//     }
-// }
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
 
         int[][] dp = new int[m + 1][n + 1];
-        // dp[i][j]: length of longest common subsequence between:
-        //            text1[0..i-1] and text2[0..j-1] (i and j represent lengths, not indices)
-        // base case:
-        // dp[0][j] = 0
-        // dp[i][0] = 0
-
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
@@ -61,3 +38,4 @@ class Solution {
         return dp[m][n];
     }
 }
+
