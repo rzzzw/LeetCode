2f593220fdf67e739fe480b072630868cf878270
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-01-31 23:00:27 -0800

    Time: 0 ms (100%), Space: 43.7 MB (14.5%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -8,24 +8,61 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/**
+brute force: 2 passes
+ */
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+//         int length = 0;
+//         ListNode cur = head;
+//         while (cur != null) {
+//             length++;
+//             cur = cur.next;
+//         }
+//         length = length - n;
+//         cur = dummy;
+//         while (length > 0) {
+//             cur = cur.next;
+//             length--;
+//         }
+//         cur.next = cur.next.next;
+//         return dummy.next;
+//     }
+    
+// }
+
+/**
+slow-fast pointer: 1 pass
+  - Move fast pointer n steps ahead
+  - Move fast and slow together
+  - When fast reaches end → slow is right before the node to delete
+ */
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        int length = 0;
-        ListNode cur = head;
-        while (cur != null) {
-            length++;
-            cur = cur.next;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // Move fast n steps ahead
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
         }
-        length = length - n;
-        cur = dummy;
-        while (length > 0) {
-            cur = cur.next;
-            length--;
+
+        // Move both until fast reaches the last node
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        cur.next = cur.next.next;
+
+        // remove the nth node from end
+        slow.next = slow.next.next;
+
         return dummy.next;
     }
-    
 }
