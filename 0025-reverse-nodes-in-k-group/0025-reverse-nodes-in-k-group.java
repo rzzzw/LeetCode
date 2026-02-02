4513b583c91272e9eace5b9c0c014f2978c655f0
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-02 15:13:31 -0800

    Time: 1 ms (37.03%), Space: 46.7 MB (12.35%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -8,31 +8,49 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
+/**
+Pointer     Meaning
+dummy       Protects head
+groupPrev   Node before current group
+kth	        Last node in the group
+groupNext	Node after the group
+ */
+
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        // base case
-        if (head == null || head.next == null) {
-            return head;
-        }
-        
-        int count = 0;
-        ListNode cur = head;
-        while(cur != null && count != k){
-            cur = cur.next;
-            count++;
-        }
-        
-        if(count == k){
-            cur = reverseKGroup(cur, k);
-            while (count-- > 0){
-                ListNode temp = head.next;
-                head.next = cur;
-                cur = head;
-                head = temp;
-            }
-            head = cur;
-        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
 
-        return head;
+        ListNode groupPrev = dummy;
+
+        while (true) {
+            // 1. find kth node
+            ListNode kth = groupPrev;
+            for (int i = 0; i < k && kth != null; i++) {
+                kth = kth.next;
+            }
+            if (kth == null) break;
+
+            ListNode groupNext = kth.next;
+
+            // 2. reverse group
+            ListNode prev = groupNext;
+            ListNode cur = groupPrev.next;
+
+            while (cur != groupNext) {
+                ListNode tmp = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = tmp;
+            }
+
+            //3. reconnect
+            ListNode tmp = groupPrev.next; // will be tail
+            groupPrev.next = kth;
+            groupPrev = tmp;
+        }
+        return dummy.next;
     }
 }
