94312aac8176d59b89ed83df5146079807982cd2
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-02 15:26:34 -0800

    Time: 0 ms (100%), Space: 47 MB (11.37%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,14 +9,15 @@
  *     }
  * }
  */
-public class Solution {
+
+class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null) {
             return false;
         }
         ListNode slow = head;
         ListNode fast = head.next;
-        while (slow != fast) {
+        while(slow != fast) {
             if (fast == null || fast.next == null) {
                 return false;
             }
