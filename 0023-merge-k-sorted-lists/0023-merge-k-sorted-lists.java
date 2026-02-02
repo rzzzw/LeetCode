b8602d0b6525fa235048ee215adb3abb1f86e5bf
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-01 23:43:07 -0800

    Time: 1 ms (99.61%), Space: 47 MB (40.59%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -31,6 +31,30 @@
         
 //     }
 // }
+// TC: O(N log k) - k is the number of lists
+// SC: O(k)
+
+// class Solution{
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         if (lists == null || lists.length == 0) {
+//             return null;
+//         }
+//         return mergeRange(lists, 0, lists.length - 1);
+//     }
+
+//     private ListNode mergeRange(ListNode[] lists, int left, int right) {
+//         if (left == right) {
+//             return lists[left];
+//         }
+//         int mid = left + (right - left) / 2;
+//         ListNode l1 = mergeRange(lists, left, mid);
+//         ListNode l2 = mergeRange(lists, mid + 1, right);
+//         return mergeTwoLists(l1, l2);
+//     }
+
+
+
+
 
 class Solution{
     public ListNode mergeKLists(ListNode[] lists) {
@@ -39,7 +63,6 @@ class Solution{
         }
         return mergeRange(lists, 0, lists.length - 1);
     }
-
     private ListNode mergeRange(ListNode[] lists, int left, int right) {
         if (left == right) {
             return lists[left];
@@ -53,7 +76,7 @@ class Solution{
     private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
-        while(l1 != null && l2 != null) {
+        while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
                 cur.next = l1;
                 l1 = l1.next;
@@ -65,10 +88,11 @@ class Solution{
         }
         if (l1 != null) {
             cur.next = l1;
-        }
-        if (l2 != null) {
+        } else {
             cur.next = l2;
         }
         return dummy.next;
     }
 }
+// TC: O(N log k) - k is the number of lists
+// SC: O(log k)
