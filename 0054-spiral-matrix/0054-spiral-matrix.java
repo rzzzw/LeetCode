aebe986ebd23571909da10fc1248dbe9c8305e6f
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-03 23:47:03 -0800

    Time: 0 ms (100%), Space: 43.1 MB (36.6%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,36 +1,40 @@
-class Solution {
+class Solution{
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
         if (matrix == null || matrix.length == 0) {
             return res;
         }
-        int top = 0; 
-        int bottom = matrix.length - 1;
-        int left = 0; 
-        int right = matrix[0].length - 1;
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1; 
 
         while (top <= bottom && left <= right) {
-            for (int col = left; col <= right; col++) {
-                res.add(matrix[top][col]);
+            // 1. top row
+            for (int j = left; j <= right; j++) {
+                res.add(matrix[top][j]);
             }
             top++;
-            for (int row = top; row <= bottom; row++) {
-                res.add(matrix[row][right]);
+
+            // 2. right column
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
             }
             right--;
+
+            // 3. bottom row
             if (top <= bottom) {
-                for (int col = right; col >= left; col--) {
-                    res.add(matrix[bottom][col]);
+                for (int j = right; j >= left; j--) {
+                    res.add(matrix[bottom][j]);
                 }
                 bottom--;
             }
-            if (right >= left) {
-                for (int row = bottom; row >= top; row--) {
-                    res.add(matrix[row][left]);
+
+            // 4. left column
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
                 }
                 left++;
             }
-            
         }
         return res;
     }
