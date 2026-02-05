070f969336956933dd55a3640bfa58ec5b028034
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-04 20:35:08 -0800

    Time: 6 ms (68.46%), Space: 46.8 MB (13.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,19 +4,48 @@ class Solution {
             return 0;
         }
         char[] input = s.toCharArray();
-        Map<Character, Integer> map = new HashMap<>();
-        int longest = 0;
+        Set<Character> set = new HashSet<>();
         int slow = 0;
+        int longest = 0;
         for (int fast = 0; fast < input.length; fast++) {
-            if (map.containsKey(input[fast])) {
-                slow = Math.max(map.get(input[fast]) + 1, slow);
+            while (set.contains(input[fast])) {
+                set.remove(input[slow]);
+                slow++;
             }
-            longest = Math.max(longest, fast - slow + 1);
-            map.put(input[fast], fast);
-        }
+            set.add(input[fast]);
+            longest = Math.max(fast - slow + 1, longest);
+        }        
         return longest;
     }
 }
+
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if (s == null || s.length() == 0) {
+//             return 0;
+//         }
+//         char[] input = s.toCharArray();
+//         Map<Character, Integer> distinct = new HashMap<>();
+//         int longest = 0;
+//         int slow = 0;
+//         for (int fast = 0; fast < input.length; fast++) {
+//             if (distinct.containsKey(input[fast])) {
+//                 slow = Math.max(distinct.get(input[fast]) + 1, slow);  // avoid move slow point step by step via this method
+//                     // Math.max()!!
+//             }    
+//             // while (distinct.containsKey(input[fast])) {
+//                 // distinct.remove(input[slow]);
+//                 // slow++;     
+//             // }
+//             longest = Math.max(longest, fast - slow + 1);
+//             distinct.put(input[fast], fast);
+//         }
+//         return longest;
+    
+//     }
+// }
+
 
 /**
     s
