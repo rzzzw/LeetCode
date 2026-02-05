93c26b00dc7137e1bcd6fcb67da3f356912a99ef
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-04 22:07:07 -0800

    Time: 20 ms (41.41%), Space: 48 MB (15.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,85 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) {
+        if (s == null || t == null || s.length() < t.length()) {
             return "";
         }
-        int[] need = new int[128];
-        int needCount = t.length();
+        Map<Character, Integer> need = new HashMap<>();  // frequency of characters in t
         for (char c : t.toCharArray()) {
-            need[c]++; // c is a char not a number, can it be wrote like this?
+            need.put(c, need.getOrDefault(c, 0) + 1);
         }
+        Map<Character, Integer> window = new HashMap<>(); // frequency of characters in current window
+        int required = need.size();
+        int formed = 0;  // how many characters meet their required frequency
 
-        int minLen = Integer.MAX_VALUE;
-        int minStart = 0;
         int left = 0;
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-            if (need[c] > 0) {
-                needCount--;
-            }
-            need[c]--;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
 
-            while (needCount == 0) {
-                int winLen = right - left + 1;
-                if (winLen < minLen) {
-                    minLen = winLen;
-                    minStart = left;
+        for (int right = 0; right < s.length(); right++) {
+            char c =s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+            if (need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()) {
+                formed++;
+            }
+
+            while (left <= right && formed == required) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left; 
                 }
                 char leftChar = s.charAt(left);
-                need[leftChar]++;
-                if(need[leftChar] > 0) {
-                    needCount++;
+                window.put(leftChar, window.get(leftChar) - 1);
+
+                if (need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)) {
+                    formed--;
                 }
                 left++;
             }
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
-
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 }
+
+
+// class Solution {
+//     public String minWindow(String s, String t) {
+//         if (s.length() < t.length()) {
+//             return "";
+//         }
+//         int[] need = new int[128];
+//         int needCount = t.length();
+//         for (char c : t.toCharArray()) {
+//             need[c]++; // c is a char not a number, can it be wrote like this?
+//         }
+
+//         int minLen = Integer.MAX_VALUE;
+//         int minStart = 0;
+//         int left = 0;
+//         for (int right = 0; right < s.length(); right++) {
+//             char c = s.charAt(right);
+//             if (need[c] > 0) {
+//                 needCount--;
+//             }
+//             need[c]--;
+
+//             while (needCount == 0) {
+//                 int winLen = right - left + 1;
+//                 if (winLen < minLen) {
+//                     minLen = winLen;
+//                     minStart = left;
+//                 }
+//                 char leftChar = s.charAt(left);
+//                 need[leftChar]++;
+//                 if(need[leftChar] > 0) {
+//                     needCount++;
+//                 }
+//                 left++;
+//             }
+//         }
+//         return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+
+//     }
+// }
 /**
     i   j
 s: AEBSCZSCESZ
