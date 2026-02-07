4826dc3508838359844afa9d081c6a664e63d5d2
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-06 21:50:17 -0800

    Time: 1 ms (99.75%), Space: 47.3 MB (97.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,65 +1,47 @@
+// /**
+// Approach 1: 两个档板， 三个区域
+// [0 ... slow-1]   → all non-zero, in original order
+// [slow ... i-1]   → all zeros
+// [i ... end]      → unexplored
+//  */
 // class Solution {
 //     public void moveZeroes(int[] nums) {
 //         if (nums == null || nums.length == 0) {
 //             return;
-//         } 
+//         }
 //         int slow = 0;
 //         for (int fast = 0; fast < nums.length; fast++) {
 //             if (nums[fast] != 0) {
-//                 if (fast != slow) {
-//                     nums[slow] = nums[fast];
-//                 }
+//                 swap(nums, fast, slow);
 //                 slow++;
-//             }     
+//             }
 //         }
-//         while (slow < nums.length) {
-//             nums[slow] = 0;
-//             slow++;
-//         }
+//     }
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
 //     }
 // }
 
-// class Solution {
-//     public void moveZeroes(int[] nums) {
-//         int l = 0;
-//         for (int i = 0; i < nums.length; i++) {
-//             if (nums[i] != 0) {
-//                 swap(nums, l, i);
-//                 l++; 
-//             } 
-//         }
-//     }
-
-//     private void swap(int[] nums, int a, int b) {
-//         int temp = nums[a];
-//         nums[a] = nums[b];
-//         nums[b] = temp;
-//     }
-// }
-
-
-
-// 两个档板， 三个区域
-// [0 ... slow-1]   → all non-zero, in original order
-// [slow ... i-1]   → all zeros
-// [i ... end]      → unexplored
+/**
+Approach 2: Overwrite + Fill Zeros
+Step 1: Copy non-zero elements forward
+Step 2: Fill the rest with zeros
+ */
 
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return;
-        }
-        int slow = 0;
-        for (int fast = 0; fast < nums.length; fast++) {
-            if (nums[fast] != 0) {
-                swap(nums, fast, slow);
-                slow++;
+        int idx = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[idx] = num;
+                idx++;
             }
         }
-    }
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        while (idx < nums.length) {
+            nums[idx] = 0;
+            idx++;
+        }
     }
 }
