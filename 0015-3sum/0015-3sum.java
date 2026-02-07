963d5df7052f03936c3ba95f144f1f9c73a763df
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-06 16:51:18 -0800

    Time: 29 ms (91.54%), Space: 59.5 MB (32.31%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,42 +1,51 @@
-// class Solution {
-//     public List<List<Integer>> threeSum(int[] nums) {
-//         List<List<Integer>> res = new ArrayList<>();
-//         if (nums == null || nums.length < 3) {
-//             return res;
-//         }
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
 
-//         Arrays.sort(nums);
-//         for (int i = 0; i < nums.length - 2; i++) {
-//             if (i > 0 && nums[i] == nums[i - 1]) {
-//                 continue;
-//             }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if(sum > 0) {
+                    right--;
+                } else {        // dry run [-2, 0, 0, 0, 2] if feel confused on this part.
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));  
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    // Move to next search window
+                    left++;
+                    right--;                    
+                }
+            }
+        }
+        return res;
+    }
+}
 
-//             int j = i + 1;
-//             int k = nums.length - 1;
-//             while (j < k) {
-//                 int sum = nums[i] + nums[j] + nums[k];
-//                 if (sum == 0) {
-//                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-//                     j++;
-//                     k--;
-//                     while (j < k && nums[j] == nums[j - 1]) {
-//                         j++;
-//                     }
-//                     while (j < k && nums[k] == nums[k + 1]) {
-//                         k--;
-//                     }
-//                 } else if (sum < 0) {
-//                     j++;
-//                 } else {
-//                     k--;
-//                 }
-//             }
-//         }
-//         return res;
-//     }
-// }
 
-/* time complexity
+
+/* 
+[-2, 0, 0, 0, 2]
+     ↑        ↑
+     left  right
+
+
+time complexity
 
         1  2  3  4  5  6  .....
         ^  
@@ -48,40 +57,3 @@
    (n-1) + (n-2) +...+ 1 = (1+n-1)(n-1)/2 => n^2
             
 */
-
-class Solution{
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length < 3) {
-            return res;
-        }
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1]) {
-                        j++;
-                    }
-                    while (j < k && nums[k] == nums[k + 1]) {
-                        k--;
-                    }
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    j++;
-                }
-            }
-            
-        }
-        return res;
-    }
-}
