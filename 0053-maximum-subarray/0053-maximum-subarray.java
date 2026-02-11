dd167aef727ccbf5c071c3a1d2a493e21b995291
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-10 23:24:04 -0800

    Time: 1 ms (99.61%), Space: 59.8 MB (99.9%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,19 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
+        if(nums == null || nums.length == 0) {
             return 0;
         }
-        int globalMax = nums[0];
+        
         int pre = nums[0];
+        int globalMax = pre;
         for (int i = 1; i < nums.length; i++) {
-            pre = Math.max(pre + nums[i], nums[i]);
-            globalMax = Math.max(globalMax, pre);
+            // if (nums[i] + pre > nums[i]) {
+            //     pre = nums[i] + pre;
+            // } else {
+            //     pre = nums[i];
+            // }
+            pre = Math.max(nums[i] + pre, nums[i]);
+            globalMax = Math.max(pre, globalMax);
         }
         return globalMax;
     }
