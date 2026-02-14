d62f3cb12d505e46fa4c11a2c0899002d1ea9515
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-13 21:55:42 -0800

    Time: 0 ms (100%), Space: 43.8 MB (99.16%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,11 +9,10 @@
  * }
  */
 
-
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode list3 = new ListNode(0); // dummy node
-        ListNode cur = list3;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
         while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
                 cur.next = list1;
@@ -24,11 +23,7 @@ class Solution {
             }
             cur = cur.next;
         }
-        if (list1 != null) {
-            cur.next = list1;
-        } else {
-            cur.next = list2;
-        }
-        return list3.next;
+        cur.next = (list1 != null) ? list1 : list2;
+        return dummy.next;
     }
 }
