766f6d477ac56be55536812dd74a9798c399836c
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-19 22:42:49 -0800

    Time: 1 ms (99.79%), Space: 43.3 MB (41.64%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -33,41 +33,18 @@ class Solution {
         boolean hasOdd = false;
         int res = 0;
         for (int n : count) {
-            if (n % 2 == 0) {
-                res += n;
-            } else {
-                res += n - 1;
+            res += (n / 2) * 2; // add largest even part
+            if (n % 2 == 1) {
                 hasOdd = true;
             }
+            // if (n % 2 == 0) {
+            //     res += n;
+            // } else {
+            //     res += n - 1;
+            //     hasOdd = true;
+            // }
         }
         return hasOdd ? (res + 1) : res;
     } 
 }
 
-// class Solution {
-//     public int longestPalindrome(String s) {
-//         int[] count = new int[128];  // ASCII size
-        
-//         // Count frequency of each character
-//         for (char c : s.toCharArray()) {
-//             count[c]++;
-//         }
-        
-//         int length = 0;
-//         boolean hasOdd = false;
-        
-//         for (int c : count) {
-//             length += (c / 2) * 2;  // add largest even part
-//             if (c % 2 == 1) {
-//                 hasOdd = true;
-//             }
-//         }
-        
-//         // If any character has odd count, we can place one in the middle
-//         if (hasOdd) {
-//             length += 1;
-//         }
-        
-//         return length;
-//     }
-// }
