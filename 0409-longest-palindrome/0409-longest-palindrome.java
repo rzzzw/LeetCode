bc2a592c399d3198e372ac889cf44eb576af251d
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-19 22:41:01 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,15 +1,38 @@
+// class Solution {
+//     public int longestPalindrome(String s) {
+//         if (s == null || s.length() == 0) {
+//             return 0;
+//         }
+//         Map<Character, Integer> map = new HashMap<>();
+//         for (char c : s.toCharArray()) {
+//             map.put(c, map.getOrDefault(c, 0) + 1);
+//         }
+//         boolean hasOdd = false;
+//         int res = 0;
+//         for (int n : map.values()) {
+//             if (n % 2 == 0) {
+//                 res += n;
+//             } else {
+//                 res += n - 1;
+//                 hasOdd = true;
+//             }
+//         }
+//         return hasOdd ? (res + 1) : res;
+//     } 
+// }
+
 class Solution {
     public int longestPalindrome(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        Map<Character, Integer> map = new HashMap<>();
+        int[] count = new int[128]
         for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            count[c]++;
         }
         boolean hasOdd = false;
         int res = 0;
-        for (int n : map.values()) {
+        for (int n : count) {
             if (n % 2 == 0) {
                 res += n;
             } else {
@@ -20,3 +43,31 @@ class Solution {
         return hasOdd ? (res + 1) : res;
     } 
 }
+
+// class Solution {
+//     public int longestPalindrome(String s) {
+//         int[] count = new int[128];  // ASCII size
+        
+//         // Count frequency of each character
+//         for (char c : s.toCharArray()) {
+//             count[c]++;
+//         }
+        
+//         int length = 0;
+//         boolean hasOdd = false;
+        
+//         for (int c : count) {
+//             length += (c / 2) * 2;  // add largest even part
+//             if (c % 2 == 1) {
+//                 hasOdd = true;
+//             }
+//         }
+        
+//         // If any character has odd count, we can place one in the middle
+//         if (hasOdd) {
+//             length += 1;
+//         }
+        
+//         return length;
+//     }
+// }
