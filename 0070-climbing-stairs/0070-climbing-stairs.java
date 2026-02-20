42397b46574d5b74001c30d89e80a36a57c3d36f
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-19 22:15:56 -0800

    Time: 0 ms (100%), Space: 42.3 MB (34.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 0) {
-            return 0;
-        }
         if (n == 1) {
             return 1;
         }
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        if (n == 2) {
+            return 2;
         }
-        return dp[n];
+        int pre = 1;
+        int cur = 2;
+        for (int i = 3; i <= n; i++) {
+            int tmp = cur + pre;
+            pre = cur;
+            cur = tmp;
+            
+        }
+        return cur;
     }
 }
 
