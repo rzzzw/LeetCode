e40b1ad8f76b7341d7303ba2a3d35a3d832fb32f
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-21 21:59:53 -0800

    Time: 0 ms (100%), Space: 44.3 MB (67.93%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,34 +9,34 @@
  * }
  */
 
-// class Solution {
-//     public ListNode reverseList(ListNode head) {
-//         if (head == null || head.next == null) {
-//             return head;
-//         }
-//         ListNode cur = head;
-//         ListNode pre = null;
-//         while (cur != null) {
-//             ListNode tmp = cur.next;
-//             cur.next = pre;
-//             pre = cur;
-//             cur = tmp;
-//         }
-//         return pre;
-//     }
-// }
-
 class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode newHead = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
     }
 }
+
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+//         ListNode newHead = reverseList(head.next);
+//         head.next.next = head;
+//         head.next = null;
+//         return newHead;
+//     }
+// }
 
 
 /**
