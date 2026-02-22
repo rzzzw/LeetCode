b41260faff1cdf5c0d23c1ad6c32369cf7a5f21b
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-21 22:35:05 -0800

    Time: 2 ms (51.12%), Space: 43.6 MB (56.51%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,31 +1,59 @@
 class Solution {
     public String addBinary(String a, String b) {
-        if (a.length() < b.length()) {
-            return addBinary(b, a);
-        }
         StringBuilder sb = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;        
         int carry = 0;
-        int j = b.length() - 1;
-        for (int i = a.length() - 1; i >= 0; i--) { // length of a should >= length of b
-            if (a.charAt(i) == '1') {
-                carry++;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
             }
-            if (j >= 0 && b.charAt(j--) == '1') { // 注意这里 j-- 的写法
-                carry++;
-                // j--; this is wrong 无论是否等于1 j都要--
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
             }
-            if (carry % 2 == 1) {
-                sb.append('1');
-            } else {
-                sb.append('0');
-            }
-            carry /= 2;
+
+            sb.append(sum % 2);
+            carry = sum / 2;
         }
-        if (carry == 1) {
-            sb.append('1');
-        }
-        sb.reverse();
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
+
+// class Solution {
+//     public String addBinary(String a, String b) {
+//         if (a.length() < b.length()) {
+//             return addBinary(b, a);
+//         }
+//         StringBuilder sb = new StringBuilder();
+//         int carry = 0;
+//         int j = b.length() - 1;
+//         for (int i = a.length() - 1; i >= 0; i--) { // length of a should >= length of b
+//             if (a.charAt(i) == '1') {
+//                 carry++;
+//             }
+//             if (j >= 0 && b.charAt(j--) == '1') { // 注意这里 j-- 的写法
+//                 carry++;
+//                 // j--; this is wrong 无论是否等于1 j都要--
+//             }
+//             if (carry % 2 == 1) {
+//                 sb.append('1');
+//             } else {
+//                 sb.append('0');
+//             }
+//             carry /= 2;
+//         }
+//         if (carry == 1) {
+//             sb.append('1');
+//         }
+//         sb.reverse();
+//         return sb.toString();
+//     }
+// }
 
