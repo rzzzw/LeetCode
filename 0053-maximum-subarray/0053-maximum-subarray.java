8a34d5924a32ea547c71ce51639de16237ff4a52
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-02-23 17:29:06 -0800

    Time: 1 ms (99.71%), Space: 76.9 MB (97.43%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,20 +1,27 @@
+/**
+dp[i] = max subarray sum ending at i
+At index i, we have two choices:
+  - Extend previous subarray
+    dp[i-1] + nums[i]
+  - Start fresh at i
+    nums[i]
+=> dp[i] = max(nums[i], dp[i-1] + nums[i])
+
+ */
+
 class Solution {
     public int maxSubArray(int[] nums) {
-        if(nums == null || nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
+        int current = nums[0];
+        int globalMax = nums[0];
         
-        int pre = nums[0];
-        int globalMax = pre;
         for (int i = 1; i < nums.length; i++) {
-            // if (nums[i] + pre > nums[i]) {
-            //     pre = nums[i] + pre;
-            // } else {
-            //     pre = nums[i];
-            // }
-            pre = Math.max(nums[i] + pre, nums[i]);
-            globalMax = Math.max(pre, globalMax);
+            current = Math.max(current + nums[i], nums[i]);
+            globalMax = Math.max(globalMax, current);
         }
         return globalMax;
     }
 }
+
