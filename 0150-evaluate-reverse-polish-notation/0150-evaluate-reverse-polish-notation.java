f2abbb0206ff727f5c1234e4ed1ac773973b540f
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-03-03 21:48:42 -0800

    Time: 21 ms (5.39%), Space: 47.3 MB (5.34%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,40 +1,78 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        if (tokens == null || tokens.length == 0) {
-            return 0;
-        }
         Deque<Integer> stack = new ArrayDeque<>();
+
         for (String token : tokens) {
-            if (isOperator(token)) {
-                int b = stack.poll();
-                int a = stack.poll();
-                int res = 0;
+            try {
+                stack.push(Integer.parseInt(token));
+            } catch (NumberFormatException e) {
+                int b = stack.pop();
+                int a = stack.pop();
 
                 switch (token) {
-                    case "+":
-                        res = a + b;
-                        break;
-                    case "-":
-                        res = a - b;
-                        break;
-                    case "*":
-                        res = a * b;
-                        break;
-                    case "/":
-                        res = a / b;  // integer division truncates toward zero
-                        break;                     
+                    case "+": stack.push(a + b); break;
+                    case "-": stack.push(a - b); break;
+                    case "*": stack.push(a * b); break;
+                    case "/": stack.push(a / b); break;
                 }
-                stack.push(res);
-            } else {
-                stack.push(Integer.parseInt(token));
             }
         }
+
         return stack.pop();
     }
-
-    private boolean isOperator(String s) {
-        return (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/"));
-    }
 }
+
+
+
+// class Solution {
+//     public int evalRPN(String[] tokens) {
+//         if (tokens == null || tokens.length == 0) {
+//             return 0;
+//         }
+//         Deque<Integer> stack = new ArrayDeque<>();
+//         for (String token : tokens) {
+//             if (isOperator(token)) {
+//                 int b = stack.poll();
+//                 int a = stack.poll();
+//                 int res = 0;
+
+//                 switch (token) {
+//                     case "+":
+//                         res = a + b;
+//                         break;
+//                     case "-":
+//                         res = a - b;
+//                         break;
+//                     case "*":
+//                         res = a * b;
+//                         break;
+//                     case "/":
+//                         res = a / b;  // integer division truncates toward zero
+//                         break;                     
+//                 }
+//                 stack.push(res);
+//             } else {
+//                 stack.push(Integer.parseInt(token));
+//             }
+//         }
+//         return stack.pop();
+//     }
+
+//     private boolean isOperator(String s) {
+//         return (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/"));
+//     }
+// }
+
+/**
+Built-in Way to Detect a Number：
+    Character.isDigit('5')  // true
+    Character.isDigit('+')  // false
+
+But Here’s the Catch： In RPN, tokens are strings, not single characters.
+    "2"
+    "-11"
+    "+"
+
+ */
 
 
