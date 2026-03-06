36b020479e0e98c597b0f3d546667ee112d039f0
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-03-05 20:18:28 -0800

    Time: 5 ms (83.68%), Space: 47 MB (66.94%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -3,71 +3,29 @@
 // Time: O(1) for all operations
 // Space: O(n)
 //  */
-// class MinStack {
-//     private Deque<Integer> stack;
-//     private Deque<Integer> minStack;
 
-//     public MinStack() {
-//         stack = new ArrayDeque<>();
-//         minStack = new ArrayDeque<>();
-//     }
-    
-//     public void push(int val) {
-//         stack.offerFirst(val);
-//         if (minStack.isEmpty()) {
-//             minStack.offerFirst(val);
-//         } else {
-//             minStack.offerFirst(Math.min(val, minStack.peek()));
-//         }
-        
-//     }
-    
-//     public void pop() {
-//         stack.pollFirst();
-//         minStack.pollFirst();
-//     }
-    
-//     public int top() {
-//         return stack.peek();
-//     }
-    
-//     public int getMin() {
-//         return minStack.peek();
-//     }
-// }
-
-/**
-One Stack(Space-Optimized)
-
- */
 class MinStack {
+
     private Deque<Integer> stack;
-    private int min;
+    private Deque<Integer> minStack;
 
     public MinStack() {
         stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
     }
 
     public void push(int val) {
-        if (stack.isEmpty()) {
-            stack.offerFirst(val);
-            min = val;
+        stack.offerFirst(val);
+        if (minStack.isEmpty()) {
+            minStack.offerFirst(val);
         } else {
-            if (val <= min) {
-                stack.offerFirst(min);
-                min = val;
-            }
-            stack.offerFirst(val);
+            minStack.offerFirst(Math.min(val, minStack.peek()));
         }
     }
 
     public void pop() {
-        if (stack.isEmpty()) return;
-        if (stack.pollFirst() == min) {
-            if (!stack.isEmpty()) {
-                min = stack.pollFirst();                
-            }
-        }
+        stack.pollFirst();
+        minStack.pollFirst();
     }
 
     public int top(){
@@ -75,9 +33,55 @@ class MinStack {
     }
 
     public int getMin() {
-        return min;
+        return minStack.peek();
     }
 }
+
+
+
+
+/**
+One Stack(Space-Optimized)
+
+ */
+// class MinStack {
+//     private Deque<Integer> stack;
+//     private int min;
+
+//     public MinStack() {
+//         stack = new ArrayDeque<>();
+//     }
+
+//     public void push(int val) {
+//         if (stack.isEmpty()) {
+//             stack.offerFirst(val);
+//             min = val;
+//         } else {
+//             if (val <= min) {
+//                 stack.offerFirst(min);
+//                 min = val;
+//             }
+//             stack.offerFirst(val);
+//         }
+//     }
+
+//     public void pop() {
+//         if (stack.isEmpty()) return;
+//         if (stack.pollFirst() == min) {
+//             if (!stack.isEmpty()) {
+//                 min = stack.pollFirst();                
+//             }
+//         }
+//     }
+
+//     public int top(){
+//         return stack.peek();
+//     }
+
+//     public int getMin() {
+//         return min;
+//     }
+// }
 
 /**
  * Your MinStack object will be instantiated and called as such:
