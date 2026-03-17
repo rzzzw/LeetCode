2e42c7e000bfa27d828fd996a29c68d23f3cd12b
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-03-16 21:51:26 -0700

    Time: 8 ms (21.63%), Space: 46.6 MB (62.48%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -14,14 +14,18 @@ Space Complexity: O(n) (worst case), O(log n) (best case for balanced trees)
 
  */
 
- class Solution {
+
+
+class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) {
             return null;
         }
+
         if (root == p || root == q) {
             return root;
         }
+
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
@@ -31,4 +35,4 @@ Space Complexity: O(n) (worst case), O(log n) (best case for balanced trees)
 
         return left != null ? left : right;
     }
- }
+}
