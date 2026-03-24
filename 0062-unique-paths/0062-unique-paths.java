14a300def0784c90bb6387ee9b43830c2dfa1a15
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-03-24 14:59:34 -0700

    Time: 0 ms (100%), Space: 42.1 MB (58.4%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -6,21 +6,35 @@
 
  */
 
+// 2D DP
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         for (int[] arr : dp) {
+//             Arrays.fill(arr, 1);
+//         } 
+//         for (int r = 1; r < m; r++) {
+//             for (int c = 1; c < n; c++) {
+//                 dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+//             }
+//         }
+//         return dp[m - 1][n - 1];
+//     }
+// }
+
+// 1D DP
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for (int[] arr : dp) {
-            Arrays.fill(arr, 1);
-        } 
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
         for (int r = 1; r < m; r++) {
             for (int c = 1; c < n; c++) {
-                dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+                dp[c] += dp[c - 1];
             }
         }
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 }
-
 
 /**
 To reach cell (i, j):
