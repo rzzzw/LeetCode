e839f0b48203d4cfd0c2a61dfa70096fae6800a1
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-02 23:25:49 -0700

    Time: 1 ms (99.97%), Space: 47.1 MB (9.33%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,45 +9,45 @@
  * }
  */
 
-// class Solution {
-//     public ListNode mergeKLists(ListNode[] lists) {
-//         if (lists == null || lists.length == 0) {
-//             return null;
-//         }
-//         return mergeRange(lists, 0, lists.length - 1);
-//     }
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return mergeRange(lists, 0, lists.length - 1);
+    }
 
-//     private ListNode mergeRange(ListNode[] lists, int left, int right) {
-//         if (left == right) {
-//             return lists[left];
-//         }
-//         int mid = left + (right - left) / 2;
-//         ListNode l1 = mergeRange(lists, left, mid);
-//         ListNode l2 = mergeRange(lists, mid + 1, right);
-//         return mergeTwoLists(l1, l2);
-//     }
+    private ListNode mergeRange(ListNode[] lists, int left, int right) {
+        if (left == right) {
+            return lists[left];
+        }
+        int mid = left + (right - left) / 2;
+        ListNode l1 = mergeRange(lists, left, mid);
+        ListNode l2 = mergeRange(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
 
-//     private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-//         ListNode dummy = new ListNode(-1);
-//         ListNode cur = dummy;
-//         while (l1 != null && l2 != null) {
-//             if (l1.val < l2.val) {
-//                 cur.next = l1;
-//                 l1 = l1.next;
-//             } else {
-//                 cur.next = l2;
-//                 l2 = l2.next;
-//             }
-//             cur = cur.next;
-//         }
-//         if (l1 != null) {
-//             cur.next = l1;
-//         } else {
-//             cur.next = l2;
-//         }
-//         return dummy.next;
-//     }
-// }
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        } else {
+            cur.next = l2;
+        }
+        return dummy.next;
+    }
+}
 /**
 TC: O(N log k)  (N = total nodes across ALL lists; k = number of lists)
     • Each merge = O(n).  (n = total number of nodes in the two lists being merged)
@@ -68,38 +68,38 @@ Think:
  */
 
 
-class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         if (lists == null || lists.length == 0) return null;
 
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(
-            (a, b) -> a.val - b.val
-        );
+//         PriorityQueue<ListNode> pq = new PriorityQueue<>(
+//             (a, b) -> a.val - b.val
+//         );
 
-        // Step 1: add all heads
-        for (ListNode node : lists) {
-            if (node != null) {
-                pq.offer(node);
-            }
-        }
+//         // Step 1: add all heads
+//         for (ListNode node : lists) {
+//             if (node != null) {
+//                 pq.offer(node);
+//             }
+//         }
 
-        ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
+//         ListNode dummy = new ListNode(0);
+//         ListNode cur = dummy;
 
-        // Step 2: process heap
-        while (!pq.isEmpty()) {
-            ListNode node = pq.poll();   // smallest node
-            cur.next = node;
-            cur = cur.next;
+//         // Step 2: process heap
+//         while (!pq.isEmpty()) {
+//             ListNode node = pq.poll();   // smallest node
+//             cur.next = node;
+//             cur = cur.next;
 
-            if (node.next != null) {
-                pq.offer(node.next);    // push next node
-            }
-        }
+//             if (node.next != null) {
+//                 pq.offer(node.next);    // push next node
+//             }
+//         }
 
-        return dummy.next;
-    }
-}
+//         return dummy.next;
+//     }
+// }
 /**
 • Heap size = k
 • Each node: 
