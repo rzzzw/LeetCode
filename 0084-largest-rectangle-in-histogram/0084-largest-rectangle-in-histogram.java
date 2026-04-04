6c30864df7eed49906b5c1a3477c030f3f0fee21
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-03 23:38:33 -0700

    Time: 23 ms (91.76%), Space: 81.1 MB (20.71%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -7,9 +7,18 @@ For every index, find the previous smaller element and the next smaller element.
 width = rightSmallerIndex - leftSmallerIndex - 1
 area  = heights[i] × width
 
-
+Time complexity: O(n)
+Space complexity: O(n)
 */
 
+/* 
+Monotonic Increasing Stack
+Stack stores indices, maintain: increasing heights
+
+When do we calculate area? 
+• When we see a smaller height. That means:
+    The previous bar can’t extend further
+**/
 class Solution {
     public int largestRectangleArea(int[] heights) {
         if (heights == null || heights.length == 0) {
@@ -18,24 +27,27 @@ class Solution {
         Deque<Integer> stack = new ArrayDeque<>();
         int maxArea = 0;
 
-        for (int i = 0; i <= heights.length; i++){
+        for (int i = 0; i <= heights.length; i++) {
             int curHeight = (i == heights.length) ? 0 : heights[i];
 
             while (!stack.isEmpty() && curHeight < heights[stack.peek()]) {
-                int height = heights[stack.pollFirst()];
+                int height = heights[stack.pop()]; //.pollFirst()
 
                 int right = i;
                 int left = stack.isEmpty() ? -1 : stack.peek();
                 int width = right - left - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
 
-                maxArea = Math.max(maxArea, width * height);
-            } 
-            stack.offerFirst(i);
+            stack.push(i); // .offerFirst()
         }
         return maxArea;
-
     }
 }
+
+
+
+
 
 
 
