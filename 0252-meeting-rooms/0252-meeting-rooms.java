1886f022007c8ef0f571ada466438a7fddaa5cde
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-08 22:56:47 -0700

    Time: 6 ms (91.63%), Space: 48.8 MB (5.5%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,3 +1,5 @@
+
+
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
         if (intervals == null || intervals.length <= 1) {
@@ -5,7 +7,7 @@ class Solution {
         }
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] < intervals[i-1][1]) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
                 return false;
             }
         }
