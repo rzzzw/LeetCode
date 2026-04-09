d01df0adea9ca665ab22d98037fca0819cdc8d22
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-09 07:54:02 -0700

    Time: 32 ms (39.6%), Space: 95.6 MB (55.79%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,20 +1,30 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
+        for (int n : nums) {
+            set.add(n);
         }
         int res = 0;
-        for (int num : set) {
-            if (!set.contains(num + 1)) {
-                int tmpRes = 1;
-                while (set.contains(num - 1)) {
-                    tmpRes++;
-                    num--;
+        for (int n : set) {
+            if (!set.contains(n + 1)) {
+                int tempRes = 1;
+                while (set.contains(n - 1)) {
+                    tempRes += 1;
+                    n--;
                 }
-                res = Math.max(res, tmpRes);
+                res = Math.max(res, tempRes);
             }
         }
         return res;
     }
 }
+
+
+/**
+Time Complexity:
+    Each number added once → O(n)
+    Each number visited once in a sequence → O(n)
+➡ Total: O(n)
+
+Space Complexity: HashSet stores all elements → O(n)
+ */
