6fe506ec85ae052f6c8d72692e52e6307e3ce53d
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-09 22:48:57 -0700

    Time: 23 ms (87.4%), Space: 65.8 MB (11.69%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -0,0 +1,40 @@
+/**
+prefix sum + hashmap
+
+Let: prefixSum[i] = sum from index 0 → i
+
+If: prefixSum[j] == prefixSum[i]
+   👉 Then: (i+1 ... j) sum = 0 
+   👉 That subarray has equal 0 and 1 ✅
+
+🔑 Strategy
+1. Convert:
+    • 0 → -1
+    • 1 → +1
+2. Use hashmap:
+    • key = prefix sum
+    • value = first occurrence index
+3. If same sum appears again:
+    • subarray between them = valid
+
+ */
+
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(); // k: v => prefix sum : first occurence index
+        map.put(0, -1);
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += (nums[i] == 0 ? -1 : 1);
+            if (map.containsKey(sum)) {
+                maxLen = Math.max(maxLen, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            } 
+        }
+        return maxLen;
+    }
+}
