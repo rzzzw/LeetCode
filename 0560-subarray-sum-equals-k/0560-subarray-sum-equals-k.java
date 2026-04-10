80a5be7f5a90a63c9fa0244d084ff330be5acd6a
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-10 00:05:54 -0700

    Time: 23 ms (94.05%), Space: 49.1 MB (15.96%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,3 +1,15 @@
+
+/**
+prefixSum[j] - k = prefixSum[i] => SUM(i+1 ... j) == k
+
+
+[1,2,3] 3
+
+{0, -1} {1, 0} {3, 1} {6, 2}
+                3-3=0  6-3=3
+
+
+ */
 class Solution {
     public int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -5,14 +17,23 @@ class Solution {
         int prefixSum = 0;
         int count = 0;
 
-        for (int num : nums) {
-            prefixSum += num;
+        for(int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
             if (map.containsKey(prefixSum - k)) {
                 count += map.get(prefixSum - k);
-            }
+            } 
             map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+           
         }
         return count;
     }
 }
+
+
+
+/**
+
+Time: O(n)
+Space: O(n) (HashMap for prefix sums)
+ */
 
