bbdd308ff2449cdc2d2584660a629930c892329d
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-16 22:22:58 -0700

    Time: 30 ms (71.91%), Space: 146 MB (76.14%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -27,45 +27,38 @@ Deque invariants (VERY IMPORTANT)
     - Front is always the maximum of the window
 
 example:
-          i
+              i
 1,3,-1,-3,5,3,6,7
 
-deque:  4
-res:    335
+deque:  6
+res:    3 3 5 5 6
+
 */
 
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums == null || nums.length == 0 || k == 0) {
             return new int[0];
-        }
+        }  
+        
         int n = nums.length;
         int[] res = new int[n - k + 1];
-        Deque<Integer> deque = new ArrayDeque<>(); // store indices
-
+        Deque<Integer> deque = new ArrayDeque<>();
+        
         for (int i = 0; i < n; i++) {
-
-            // 1. remove indices out of window
             if (!deque.isEmpty() && deque.peekFirst() <= i - k) {
-                deque.pollFirst(); 
+                deque.pollFirst();
             }
-
-            // 2. Maintain decreasing order
-            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
                 deque.pollLast();
             }
-
-            // 3. Add current index
             deque.offerLast(i);
-
-            // 4. Record max when window is formed
             if (i >= k - 1) {
                 res[i - k + 1] = nums[deque.peekFirst()];
             }
+           
         }
         return res;
-
-
     }
 }
 
