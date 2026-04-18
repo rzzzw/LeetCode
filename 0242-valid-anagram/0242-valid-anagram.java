2ddbc2273e4e60c1c21ae4ed0304d8ee39a14d34
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-17 23:12:19 -0700

    Time: 5 ms (61.74%), Space: 44 MB (97.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,8 +6,6 @@ class Solution {
         int[] dict = new int[26];
         for (int i = 0; i < s.length(); i++) {
             dict[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < t.length(); i++) {
             dict[t.charAt(i) - 'a']--;
         }
         for (int i = 0; i < dict.length; i++) {
