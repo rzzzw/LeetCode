57c5e725b4ad9c43dbaf01137dd9d843c6363c1b
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-17 23:12:00 -0700

    Time: 3 ms (92.63%), Space: 44.4 MB (76.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,19 @@
-class Solution{
+class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int[] map = new int[26];
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] dict = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            map[s.charAt(i) - 'a']++;
+            dict[s.charAt(i) - 'a']++;
         }
         for (int i = 0; i < t.length(); i++) {
-            map[t.charAt(i) - 'a']--;
+            dict[t.charAt(i) - 'a']--;
         }
-        for (int i : map) {
-            if (i != 0) return false;
+        for (int i = 0; i < dict.length; i++) {
+            if (dict[i] != 0) {
+                return false;
+            }
         }
         return true;
     }
