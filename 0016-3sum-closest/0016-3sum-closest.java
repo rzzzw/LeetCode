7ae25aaea121a42b7bfbb0bf4cd88942506380b9
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-17 21:49:06 -0700

    Time: 17 ms (78%), Space: 45.4 MB (83.46%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -2,7 +2,6 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int n = nums.length;
-
         int closest = nums[0] + nums[1] + nums[2];
 
         for (int i = 0; i < n - 2; i++) {
@@ -10,12 +9,12 @@ class Solution {
             int right = n - 1;
 
             while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
 
-                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(sum - target) < Math.abs(target - closest)) {
                     closest = sum;
                 }
-
+            
                 if (sum < target) {
                     left++;
                 } else if (sum > target) {
@@ -23,8 +22,12 @@ class Solution {
                 } else {
                     return target;
                 }
+
             }
+
         }
         return closest;
+        
     }
 }
+
