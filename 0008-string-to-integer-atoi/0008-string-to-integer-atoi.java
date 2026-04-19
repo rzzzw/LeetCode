97c4912dcf262c7507b5c3ba5e0552e0c2488aaf
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-19 16:39:12 -0700

    Time: 1 ms (100%), Space: 43.9 MB (45.18%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -3,71 +3,35 @@ class Solution {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        char[] ch = s.toCharArray();
+        char[] ch = s.toCharArray(); 
+        int i = 0;
+        
         int n = ch.length;
 
-        // Spaces
-        int i = 0;
+        // 1. skip spaces
         while (i < n && ch[i] == ' ') {
             i++;
         }
 
-        // Sign
+        // 2. Sign
         int sign = 1;
-        if (i < n && (ch[i] == '+' || ch[i] == '-')) {
-            sign = (ch[i] == '+') ? 1 : -1;
+        if (i < n && (ch[i] == '-' || ch[i] ==  '+')) { // !! can't be while loop here.  Test case: "+-12" Expected answer: 0
+            sign = (ch[i] == '-') ? -1 : 1;
             i++;
         }
 
+        // 3. Convert digits
         int num = 0;
-        while (i < n && Character.isDigit(ch[i])) {
+        while (i < ch.length && (ch[i] - '0') >= 0 && (ch[i] - '0') <= 9) { // Character.isDigit(ch[i])
             int digit = ch[i] - '0';
-            if ((Integer.MAX_VALUE - digit) / 10 < num) {
-                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            if (num > (Integer.MAX_VALUE - digit) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
+            // num *= 10;
+            // num += (ch[i] - '0');
             num = num * 10 + digit;
             i++;
         }
-        
         return num * sign;
     }
 }
-
-
-// class Solution {
-//     public int myAtoi(String s) {
-//         if (s == null || s.length() == 0) {
-//             return 0;
-//         }
-//         char[] ch = s.toCharArray(); 
-//         int i = 0;
-        
-//         int n = ch.length;
-
-//         // 1. skip spaces
-//         while (i < n && ch[i] == ' ') {
-//             i++;
-//         }
-
-//         // 2. Sign
-//         int sign = 1;
-//         if (i < n && (ch[i] == '-' || ch[i] ==  '+')) { // !! can't be while loop here.  Test case: "+-12" Expected answer: 0
-//             sign = (ch[i] == '-') ? -1 : 1;
-//             i++;
-//         }
-
-//         // 3. Convert digits
-//         int num = 0;
-//         while (i < ch.length && (ch[i] - '0') >= 0 && (ch[i] - '0') <= 9) { // Character.isDigit(ch[i])
-//             int digit = ch[i] - '0';
-//             if (num > (Integer.MAX_VALUE - digit) / 10) {
-//                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-//             }
-//             // num *= 10;
-//             // num += (ch[i] - '0');
-//             num = num * 10 + digit;
-//             i++;
-//         }
-//         return num * sign;
-//     }
-// }
