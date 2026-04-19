513e3da68bbf0c02b4597b6fcd36839a7cd52728
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-18 22:45:09 -0700

    Time: 2 ms (99.94%), Space: 45.4 MB (85.97%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,88 +1,92 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() < t.length()) {
+        if (s.length() < t.length()) {
             return "";
         }
-        Map<Character, Integer> need = new HashMap<>();  // frequency of characters in t
+        int[] need = new int[128];
+        int needCount = t.length();
         for (char c : t.toCharArray()) {
-            need.put(c, need.getOrDefault(c, 0) + 1);
+            need[c]++;
         }
-        Map<Character, Integer> window = new HashMap<>(); // frequency of characters in current window
-        int required = need.size();
-        int formed = 0;  // how many characters meet their required frequency
 
-        int left = 0;
         int minLen = Integer.MAX_VALUE;
-        int start = 0;
-
+        int minStart = 0;
+        int left = 0;
         for (int right = 0; right < s.length(); right++) {
-            char c =s.charAt(right);
-            window.put(c, window.getOrDefault(c, 0) + 1);
-            if (need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()) {
-                formed++;
+            char c = s.charAt(right);
+            if (need[c] > 0) {
+                needCount--;
             }
+            need[c]--;
 
-            while (left <= right && formed == required) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    start = left; 
+            while (needCount == 0) {
+                int winLen = right - left + 1;
+                if (winLen < minLen) {
+                    minLen = winLen;
+                    minStart = left;
                 }
-                char leftChar = s.charAt(left);
-                window.put(leftChar, window.get(leftChar) - 1);
 
-                if (need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)) {
-                    formed--;
+                char leftChar = s.charAt(left);
+                need[leftChar]++;
+                if (need[leftChar] > 0) {
+                    needCount++;
                 }
                 left++;
             }
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
     }
 }
 
 
-// class Solution {
-//     public String minWindow(String s, String t) {
-//         if (s.length() < t.length()) {
-//             return "";
-//         }
-//         int[] need = new int[128];
-//         int needCount = t.length();
-//         for (char c : t.toCharArray()) {
-//             need[c]++; // c is a char not a number, can it be wrote like this?
-//         }
 
-//         int minLen = Integer.MAX_VALUE;
-//         int minStart = 0;
-//         int left = 0;
-//         for (int right = 0; right < s.length(); right++) {
-//             char c = s.charAt(right);
-//             if (need[c] > 0) {
-//                 needCount--;
-//             }
-//             need[c]--;
-
-//             while (needCount == 0) {
-//                 int winLen = right - left + 1;
-//                 if (winLen < minLen) {
-//                     minLen = winLen;
-//                     minStart = left;
-//                 }
-//                 char leftChar = s.charAt(left);
-//                 need[leftChar]++;
-//                 if(need[leftChar] > 0) {
-//                     needCount++;
-//                 }
-//                 left++;
-//             }
-//         }
-//         return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
-
-//     }
-// }
 /**
     i   j
 s: AEBSCZSCESZ
 t: ZES
 
  */
+
+
+
+// class Solution {
+//     public String minWindow(String s, String t) {
+//         if (s == null || t == null || s.length() < t.length()) {
+//             return "";
+//         }
+//         Map<Character, Integer> need = new HashMap<>();  // frequency of characters in t
+//         for (char c : t.toCharArray()) {
+//             need.put(c, need.getOrDefault(c, 0) + 1);
+//         }
+//         Map<Character, Integer> window = new HashMap<>(); // frequency of characters in current window
+//         int required = need.size();
+//         int formed = 0;  // how many characters meet their required frequency
+
+//         int left = 0;
+//         int minLen = Integer.MAX_VALUE;
+//         int start = 0;
+
+//         for (int right = 0; right < s.length(); right++) {
+//             char c =s.charAt(right);
+//             window.put(c, window.getOrDefault(c, 0) + 1);
+//             if (need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()) {
+//                 formed++;
+//             }
+
+//             while (left <= right && formed == required) {
+//                 if (right - left + 1 < minLen) {
+//                     minLen = right - left + 1;
+//                     start = left; 
+//                 }
+//                 char leftChar = s.charAt(left);
+//                 window.put(leftChar, window.get(leftChar) - 1);
+
+//                 if (need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)) {
+//                     formed--;
+//                 }
+//                 left++;
+//             }
+//         }
+//         return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+//     }
+// }
