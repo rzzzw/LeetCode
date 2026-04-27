049fdf9c7f615da85d3b45c47e525fd6e57bf301
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-27 16:30:20 -0700

    Time: 172 ms (77.53%), Space: 42.7 MB (85.07%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -2,10 +2,11 @@ class Solution {
     public void solveSudoku(char[][] board) {
         dfs(board, 0, 0);
     }
+
     private boolean dfs(char[][] board, int x, int y) {
         if (x == 9) return true;
         if (y == 9) {
-           return dfs(board, x + 1, 0);
+            return dfs(board, x + 1, 0);
         }
         if (board[x][y] != '.') {
             return dfs(board, x, y + 1);
@@ -22,15 +23,16 @@ class Solution {
         return false;
     }
 
-    private boolean isValid(char[][] board, int x, int y, char c){
-        for (int row = 0; row < 9; row++) {
-            if (board[row][y] == c) return false;
+    private boolean isValid(char[][] board, int x, int y, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[x][i] == c) return false;
         }
-        for (int col = 0; col < 9; col++) {
-            if (board[x][col] == c) return false;
+        for (int i = 0; i < 9; i++) {
+            if (board[i][y] == c) return false;
         }
         int smallRow = x / 3 * 3;
         int smallCol = y / 3 * 3;
+
         for (int i = smallRow; i < smallRow + 3; i++) {
             for (int j = smallCol; j < smallCol + 3; j++) {
                 if (board[i][j] == c) return false;
