d16939c5682c12c4009b472442b41195e96dffc8
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-04-27 22:19:20 -0700

    Time: 13 ms (99.88%), Space: 41.8 MB (93.21%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -8,12 +8,11 @@ public class Solution extends VersionControl {
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (isBadVersion(mid)) {
-                right = mid;
+                right = mid; 
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        return right;
     }
 }
-
