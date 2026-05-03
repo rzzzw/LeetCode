b8a91dba24528051d93c90698b478420410d6889
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-05-03 16:03:30 -0700

    Time: 0 ms (100%), Space: 46.9 MB (42.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,31 +1,31 @@
 class Solution {
-    private static final int[][] DIRS = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    public static final int[][] DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int original = image[sr][sc];
         if (original == color) {
             return image;
         }
-        dfs(image, sr, sc, original, color);
+        dfs(image, sr, sc, color, original);
         return image;
     }
-
-    private void dfs(int[][] image, int r, int c, int original, int color) {
+    private void dfs(int[][] image, int r, int c, int color, int original) {
         // boundary check
-        if (r < 0 || c < 0 || r >= image.length || c >= image[0].length) {
+        if (r < 0 || r >= image.length || c < 0 || c >= image[0].length) {
             return;
         }
-        
+
         // only fill cells with original color
         if (image[r][c] != original) {
             return;
         }
 
         image[r][c] = color;
-
+        
         // explore 4 directions
         for (int[] d : DIRS) {
-            dfs(image, r + d[0], c + d[1], original, color);
+            dfs(image, r + d[0], c + d[1], color, original);
         }
     }
 }
+
 
