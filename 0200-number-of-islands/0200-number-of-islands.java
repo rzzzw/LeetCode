397a81e4e4ad109107d1c69bbc8fdb2487365061
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-05-05 15:12:08 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,5 +1,6 @@
 class Solution {
-    private static final int[][] DIRS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    private static final int[][] DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
@@ -10,7 +11,7 @@ class Solution {
         int islands = 0;
 
         for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
+            for (int c = 0; c < cols; c++){
                 if (grid[r][c] == '1') {
                     islands++;
                     dfs(grid, r, c);
@@ -21,18 +22,23 @@ class Solution {
     }
 
     private void dfs(char[][] grid, int r, int c) {
-        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0') {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '0') {
             return;
-        }
+        } 
 
         grid[r][c] = '0';
-        
+
         for (int[] d : DIRS) {
+
             dfs(grid, r + d[0], c + d[1]);
+
         }
     }
-
 }
+
+
+
+
 
 
 // why not dfs(grid, r, c, islands) and return islands[0] in this problem?
