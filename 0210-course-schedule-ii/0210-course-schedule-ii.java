460ab912b8e61f8ac0f7eabe6620634d06565aea
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-05-19 16:17:09 -0700

    Time: 5 ms (84.42%), Space: 46.7 MB (68.64%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -21,7 +21,7 @@ class Solution {
 
         Queue<Integer> q = new ArrayDeque<>();
 
-        // put indegree 0 to Q 
+        // add indegree 0 courses
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
                 q.offer(i);
@@ -43,66 +43,12 @@ class Solution {
             }
         }
 
-        return idx == numCourses ? order : new int[0];
+        // cycle exists
+        if (idx != numCourses) {
+            return new int[0];
+        }
+
+        return order;
     }
 }
 
-// class Solution {
-
-//     public int[] findOrder(int numCourses, int[][] prerequisites) {
-
-//         List<List<Integer>> graph = new ArrayList<>();
-
-//         for (int i = 0; i < numCourses; i++) {
-//             graph.add(new ArrayList<>());
-//         }
-
-//         int[] indegree = new int[numCourses];
-
-//         // build graph
-//         for (int[] p : prerequisites) {
-
-//             int course = p[0];
-//             int prereq = p[1];
-
-//             graph.get(prereq).add(course);
-
-//             indegree[course]++;
-//         }
-
-//         Queue<Integer> q = new ArrayDeque<>();
-
-//         // add indegree 0 courses
-//         for (int i = 0; i < numCourses; i++) {
-//             if (indegree[i] == 0) {
-//                 q.offer(i);
-//             }
-//         }
-
-//         int[] order = new int[numCourses];
-//         int idx = 0;
-
-//         while (!q.isEmpty()) {
-
-//             int cur = q.poll();
-
-//             order[idx++] = cur;
-
-//             for (int nei : graph.get(cur)) {
-
-//                 indegree[nei]--;
-
-//                 if (indegree[nei] == 0) {
-//                     q.offer(nei);
-//                 }
-//             }
-//         }
-
-//         // cycle exists
-//         if (idx != numCourses) {
-//             return new int[0];
-//         }
-
-//         return order;
-//     }
-// }
