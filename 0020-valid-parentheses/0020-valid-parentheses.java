fd8fc6152709c2a830f10adff5a2dbc817666ad5
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-07-09 08:33:11 -0700

    Time: 3 ms (87.15%), Space: 43.5 MB (26.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,30 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s == null || s.length() == 0) {
+        if(s == null || s.length() == 0) {
             return true;
         }
 
-        if (s.length() % 2 == 1) {
+        if (s.length() % 2 != 0) {
             return false;
         }
 
         Deque<Character> stack = new ArrayDeque<>();
         for (char c : s.toCharArray()) {
             if (c == '(') {
-                stack.offerFirst(')');
-            } else if (c == '{') {
-                stack.offerFirst('}');
+                stack.push(')');
             } else if (c == '[') {
-                stack.offerFirst(']');
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                } else if (stack.pollFirst() != c) {
-                    return false;
-                }
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.poll() != c){
+                return false;
             }
         }
         return stack.isEmpty();
     }
 }
-
