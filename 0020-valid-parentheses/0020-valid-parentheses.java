4d62033384793abd8775718741bd936f083b6f10
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-07-09 08:40:29 -0700

    Time: 3 ms (87.15%), Space: 43.3 MB (41.26%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -16,10 +16,39 @@ class Solution {
                 stack.push(']');
             } else if (c == '{') {
                 stack.push('}');
-            } else if (stack.isEmpty() || stack.poll() != c){
-                return false;
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (stack.poll() != c) {
+                    return false;
+                }
             }
         }
         return stack.isEmpty();
     }
 }
+
+/**
+1. Clarity
+    • The input is a string containing only parentheses characters ()[]{}.
+    • I should return whether every opening bracket has a correctly matched closing bracket in the correct order.
+    • Can I assume the input only contains these six characters?
+    ...
+2. Example
+3. Initial intuition
+    My first thought is that whenever I see a closing bracket, I need to know the most recent unmatched opening bracket.That "most recent" behavior sounds like a stack.
+
+4. Explain the algorithm
+    I'll scan from left to right.
+    Every opening bracket goes onto the stack.
+    When I encounter a closing bracket, it must match the current stack top.
+    If the stack is empty or doesn't match, I can immediately return false.
+    At the end, the stack must also be empty.
+
+5. Dry run --> "{[]}"
+6. Complexity
+     Time O(n); Space O(n).
+7. Code
+
+
+ */
