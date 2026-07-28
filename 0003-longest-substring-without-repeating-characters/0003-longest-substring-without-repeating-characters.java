399e679917d4a6f8b3ad7e88cc317c8504f28f4b
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-07-28 12:09:23 -0700

    Time: 7 ms (37.55%), Space: 46.3 MB (48.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,68 +1,3 @@
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         if (s == null || s.length() == 0) {
-//             return 0;
-//         }
-//         int slow = 0;
-//         Set<Character> set = new HashSet<>();
-//         int longest = 0;
-//         for (int fast = 0; fast < s.length(); fast++) {
-//             while (set.contains(s.charAt(fast))) {
-//                 set.remove(s.charAt(slow));
-//                 slow++;
-//             }
-//             set.add(s.charAt(fast));
-//             longest = Math.max(longest, fast - slow + 1);
-//         }
-//         return longest;
-//     }
-// }
-
-
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         if (s == null || s.length() == 0) {
-//             return 0;
-//         }
-//         char[] input = s.toCharArray();
-//         Map<Character, Integer> distinct = new HashMap<>();
-//         int longest = 0;
-//         int slow = 0;
-//         for (int fast = 0; fast < input.length; fast++) {
-//             if (distinct.containsKey(input[fast])) {
-//                 slow = Math.max(distinct.get(input[fast]) + 1, slow);  // avoid move slow point step by step via this method
-//                     // Math.max()!!
-//             }    
-//             // while (distinct.containsKey(input[fast])) {
-//                 // distinct.remove(input[slow]);
-//                 // slow++;     
-//             // }
-//             longest = Math.max(longest, fast - slow + 1);
-//             distinct.put(input[fast], fast);
-//         }
-//         return longest;
-    
-//     }
-// }
-
-
-/**
-    s
-    abcabcbb
-       f
-
-      s
-    abba
-       f
-longest = 2
-map:
-    a: 0
-    b: 2
-
- */
-
-
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0) {
@@ -70,20 +5,15 @@ class Solution {
         }
         char[] arr = s.toCharArray();
         Set<Character> set = new HashSet<Character>();
-        int longest = 1;
-        int l = 0;   
-        int r = 1;
-        set.add(arr[l]);    
-        while(r < arr.length) {
-            char c = arr[r];
-            if (!set.contains(c)) {
-                set.add(c);
-                longest = Math.max(r - l + 1, longest);
-                r++;
-            } else {
+        int longest = 0;
+        int l = 0;     
+        for (int r = 0; r < arr.length; r++) {
+            while (set.contains(arr[r])) {
                 set.remove(arr[l]);
                 l++;
             }
+            set.add(arr[r]);
+            longest = Math.max(r - l + 1, longest);
         }
         return longest;
     }
