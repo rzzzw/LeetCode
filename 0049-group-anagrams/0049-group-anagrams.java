1f15abe945ffdeeca483d6c90753a5a4f448d6de
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-07-29 16:01:15 -0700

    Time: 7 ms (72.67%), Space: 49.6 MB (58.71%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -7,39 +7,39 @@ Complexity:
 
 
  */
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return new ArrayList<>();
-        }
-        Map<String, List<String>> map = new HashMap<>();
-        for (String s : strs) {
-            int[] count = new int[26];
-            for (char c : s.toCharArray()) {
-                count[c - 'a']++;
-            }
-            StringBuilder sb = new StringBuilder();
-            for (int n : count) {
-                sb.append(n).append('_');
-            }
-            String key = sb.toString();
-            // option 1:
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         if (strs == null || strs.length == 0) {
+//             return new ArrayList<>();
+//         }
+//         Map<String, List<String>> map = new HashMap<>();
+//         for (String s : strs) {
+//             int[] count = new int[26];
+//             for (char c : s.toCharArray()) {
+//                 count[c - 'a']++;
+//             }
+//             StringBuilder sb = new StringBuilder();
+//             for (int n : count) {
+//                 sb.append(n).append('_');
+//             }
+//             String key = sb.toString();
+//             // option 1:
+//             map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
 
-            // option 2:
-            // map.putIfAbsent(key, new ArrayList<>());
-            // map.get(key).add(s);
+//             // option 2:
+//             // map.putIfAbsent(key, new ArrayList<>());
+//             // map.get(key).add(s);
 
-            // option 3:       
-            // if (!map.containsKey(key)) {
-            //     map.put(key, new ArrayList<>());
-            // } 
-            // map.get(key).add(s);
+//             // option 3:       
+//             // if (!map.containsKey(key)) {
+//             //     map.put(key, new ArrayList<>());
+//             // } 
+//             // map.get(key).add(s);
             
-        }
-        return new ArrayList<>(map.values());
-    }
-}
+//         }
+//         return new ArrayList<>(map.values());
+//     }
+// }
 
 /**
 Method 1:
@@ -78,3 +78,18 @@ Method 1:
 //     }
 // }
 
+class Solution{
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = new String(c);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
