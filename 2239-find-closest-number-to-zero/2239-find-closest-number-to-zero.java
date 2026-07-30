3278e831e0541774e86d54c9aaf75d823277c200
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-07-30 16:43:55 -0700

    Time: 1 ms (100%), Space: 46.8 MB (61.18%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
@@ -1,17 +1,37 @@
+// class Solution {
+//     public int findClosestNumber(int[] nums) {
+//         int resAb = Integer.MIN_VALUE;
+//         int res = Integer.MAX_VALUE;
+//         for (int n : nums) {
+//             if (Math.abs(n) <= Math.abs(res)) {
+//                 res = n;
+//                 if (n > 0) {
+//                     resAb = n;
+//                 }
+//             }
+//         }
+//         if (Math.abs(res) == Math.abs(resAb)) {
+//             return Math.max(res, resAb);
+//         }
+//         return res;
+//     }
+// }
+
 class Solution {
     public int findClosestNumber(int[] nums) {
-        int resAb = Integer.MIN_VALUE;
+        int minA = Integer.MAX_VALUE;
         int res = Integer.MAX_VALUE;
         for (int n : nums) {
-            if (Math.abs(n) <= Math.abs(res)) {
-                res = n;
-                if (n > 0) {
-                    resAb = n;
-                }
+            int curA = n;
+            if (curA < 0) {
+                curA *= -1;
             }
-        }
-        if (Math.abs(res) == Math.abs(resAb)) {
-            return Math.max(res, resAb);
+            if (curA < minA) {
+                minA = curA;
+                res = n;
+            } else if(curA == minA) {
+                res = Math.max(n, res);
+            }
         }
         return res;
     }
