2efe7c1039c508c52f6d005c13b8d58d63ea8666
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-07-31 16:54:32 -0700

    Time: 2 ms (93.95%), Space: 72.3 MB (16.53%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,23 +1,24 @@
-class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int n = nums.length;
+//         int[] res = new int[n];
 
-        res[0] = 1;
-        for (int i = 1; i < n; i++) {
-            res[i] = nums[i - 1] * res[i - 1];
-        }
-        int right = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            res[i] = right * res[i];
-            right *= nums[i];
-        }
-        return res;
-    }
-}
+//         res[0] = 1;
+//         for (int i = 1; i < n; i++) {
+//             res[i] = nums[i - 1] * res[i - 1];
+//         }
+//         int right = 1;
+//         for (int i = n - 1; i >= 0; i--) {
+//             res[i] = right * res[i];
+//             right *= nums[i];
+//         }
+//         return res;
+//     }
+// }
 
 
 /**
+After the left pass, each element in that array holds the product of all previous elements. After the right pass, each element holds the product of all following elements.
 
 product_except_i = (product of all elements to the LEFT of i) × (product of all elements to the RIGHT of i)
 
@@ -31,3 +32,18 @@ product_except_i = (product of all elements to the LEFT of i) × (product of all
 
  */
 
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        } 
+        int a = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            a *= nums[i + 1];
+            res[i] *= a;
+        }
+        return res;
+    }
+}
