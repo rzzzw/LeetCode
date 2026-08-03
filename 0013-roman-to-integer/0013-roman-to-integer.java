c710814283e31942113531902356bb7e6f5d889d
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-03 09:47:09 -0700

    Time: 6 ms (7.79%), Space: 47 MB (9.6%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,97 @@
+// class Solution {
+//     public int romanToInt(String s) {
+//         Map<Character, Integer> map = new HashMap<>();
+//         map.put('I', 1);
+//         map.put('V', 5);
+//         map.put('X', 10);
+//         map.put('L', 50);
+//         map.put('C', 100);
+//         map.put('D', 500);
+//         map.put('M', 1000);
+
+//         int res = 0;
+
+//         for(int i = s.length() - 1; i >= 0; i--){
+//             int num = map.get(s.charAt(i));
+//             if (4 * num < res) {
+//                 res -= num;
+//             } else {
+//                 res += num;
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+
+/**
+DXCI
+500 10 100 1
+CMXCIV
+100 1000 10 100 1 5
+
+VL
+5 50
+
+
+class Solution {
+    public int romanToInt(String s) {
+        int num=0,ans=0;
+        for(int i=s.length()-1;i>=0;i--){
+            switch(s.charAt(i)){
+                case 'I' -> num=1;
+                case 'V' -> num=5;
+                case 'X' -> num=10;
+                case 'L' -> num=50;
+                case 'C' -> num=100;
+                case 'D' -> num=500;
+                case 'M' -> num=1000;
+            }
+            if(4*num<ans){
+                ans-=num;
+            }
+            else{ans+=num;}
+        }
+        return ans;
+    }
+}
+
+ */
+
+
+ /**
+ input: Roman number to Integer coversion, the Roman number is represented by a Sting made with (I, V, X, L, C, D, M); output is a int
+    map -> Roman number Character : int
+    1' characters:  I, X, C
+    2' 
+
+
+ 
+  */
+
+  class Solution {
+    public int romanToInt(String s) {
+
+        Map<Character,Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int res = 0;
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() && map.get(ch[i]) < map.get(ch[i + 1])) {
+                res += (map.get(ch[i + 1]) - map.get(ch[i]));
+                i++;
+            } else {
+                res += map.get(ch[i]);
+            }
+        }
+        return res;
+    }   
+  }
