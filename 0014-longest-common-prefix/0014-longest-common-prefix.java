5d1015ae3d28a8380a43f343230b7fb8362efafd
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-04 12:37:40 -0700

    Time: 1 ms (61.25%), Space: 43.2 MB (51.18%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,26 +1,3 @@
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return null;
-        }
-
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            String cur = strs[i];
-            int idx = 0;
-            while (idx < prefix.length() && idx < cur.length()) {
-                if (prefix.charAt(idx) == cur.charAt(idx)) {
-                    idx++;
-                } else {
-                    break;
-                }
-            }
-            prefix = prefix.substring(0, idx);
-        }
-        return prefix;
-    }
-}
-
 // class Solution {
 //     public String longestCommonPrefix(String[] strs) {
 //         if (strs == null || strs.length == 0) {
@@ -40,3 +17,25 @@ class Solution {
 //         return base;
 //     }
 // }
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return null;
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            String cur = strs[i];
+            int idx = 0;
+            while (idx < prefix.length() && idx < cur.length()) {
+                if (prefix.charAt(idx) == cur.charAt(idx)) {
+                    idx++;
+                } else {
+                    break;
+                }
+            }
+            prefix = prefix.substring(0, idx);
+        }
+        return prefix;
+    }
+}
