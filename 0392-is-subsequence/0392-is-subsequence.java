4315d8c64230f3634ee62227517039a5379398cd
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-04 10:14:22 -0700

    Time: 1 ms (95.57%), Space: 43.1 MB (11.28%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        // assume s != null && t != null
+        if (s.length() == 0) {
+            return true;
+        }
+        if (s.length() > t.length()) {
+            return false;
+        }
+        int i = 0, j = 0;
+        int count = 0;
+        while (i < s.length() && j < t.length()) {
+
+            while (j < t.length() && t.charAt(j) != s.charAt(i)) {
+                j++;
+            }
+            if (j < t.length() && s.charAt(i) == t.charAt(j)) {
+                count++;
+            }            
+            i++;
+            j++;        
+            if (i == s.length() && count == s.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
