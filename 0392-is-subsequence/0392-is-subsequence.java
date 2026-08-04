e5d0b2648eb719514024f1d0a64cd485fcad16e5
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-04 10:49:21 -0700

    Time: 2 ms (73.85%), Space: 42.7 MB (63.49%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,52 +1,22 @@
-// class Solution {
-//     public boolean isSubsequence(String s, String t) {
-//         // assume s != null && t != null
-//         if (s.length() == 0) {
-//             return true;
-//         }
-//         if (s.length() > t.length()) {
-//             return false;
-//         }
-//         int i = 0, j = 0;
-//         while (i < s.length() && j < t.length()) {
-
-//             if (s.charAt(i) == t.charAt(j)) {
-//                 i++;
-//             }            
-//             j++;    
-//         }
-//         return i == s.length();
-//     }
-// }
-
-
 class Solution {
-
     public boolean isSubsequence(String s, String t) {
-
-        Integer sourceLen = s.length(), targetLen = t.length();
-        // the source string is empty
-        if (sourceLen == 0)
+        // assume s != null && t != null
+        if (s.length() == 0) {
             return true;
-
-        int[][] dp = new int[sourceLen + 1][targetLen + 1];
-        // DP calculation, we fill the matrix column by column, bottom up
-        for (int col = 1; col <= targetLen; ++col) {
-            for (int row = 1; row <= sourceLen; ++row) {
-                if (s.charAt(row - 1) == t.charAt(col - 1))
-                    // find another match
-                    dp[row][col] = dp[row - 1][col - 1] + 1;
-                else
-                    // retrieve the maximal result from previous prefixes
-                    dp[row][col] = Math.max(dp[row][col - 1], dp[row - 1][col]);
-            }
-            // check if we can consume the entire source string,
-            // with the current prefix of the target string.
-            if (dp[sourceLen][col] == sourceLen)
-                return true;
         }
+        if (s.length() > t.length()) {
+            return false;
+        }
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
 
-        // matching failure
-        return false;
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }            
+            j++;    
+        }
+        return i == s.length();
     }
 }
+
+
