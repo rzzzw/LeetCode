10ee75cb1cb411257a401146437ff63a5c56645e
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-04 10:47:37 -0700

    Time: 2 ms (73.85%), Space: 42.9 MB (26.17%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -8,21 +8,42 @@ class Solution {
             return false;
         }
         int i = 0, j = 0;
-        int count = 0;
         while (i < s.length() && j < t.length()) {
 
-            while (j < t.length() && t.charAt(j) != s.charAt(i)) {
-                j++;
-            }
-            if (j < t.length() && s.charAt(i) == t.charAt(j)) {
-                count++;
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
             }            
-            i++;
-            j++;        
-            if (i == s.length() && count == s.length()) {
-                return true;
-            }
+            j++;    
         }
-        return false;
+        return i == s.length();
     }
 }
+
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         // assume s != null && t != null
+//         if (s.length() == 0) {
+//             return true;
+//         }
+//         if (s.length() > t.length()) {
+//             return false;
+//         }
+//         int i = 0, j = 0;
+//         int count = 0;
+//         while (i < s.length() && j < t.length()) {
+
+//             while (j < t.length() && t.charAt(j) != s.charAt(i)) {
+//                 j++;
+//             }
+//             if (j < t.length() && s.charAt(i) == t.charAt(j)) {
+//                 count++;
+//             }            
+//             i++;
+//             j++;        
+//             if (i == s.length() && count == s.length()) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
