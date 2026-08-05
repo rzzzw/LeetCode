13238c4a56c4b94dcb5d05e7d758f97fb663796e
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-05 06:23:26 -0700

    Time: 8 ms (89.03%), Space: 47.1 MB (66.22%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -21,17 +21,16 @@ Why This Is a DAG？
     Edges only go: smaller -> larger. You can NEVER return to a smaller value. So cycles are impossible. That makes DFS + memoization safe.
  */
 
-
 class Solution {
 
-    public static final int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public static final int[][] DIRS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public int longestIncreasingPath(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
         int m = matrix.length;
-        int n = matrix[0].length;
+        int n = matrix[0].length;      
 
         int[][] dp = new int[m][n];
 
@@ -40,20 +39,17 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 res = Math.max(res, dfs(matrix, i, j, dp));
             }
-        }
+        }  
         return res;
     }
 
     private int dfs(int[][] matrix, int r, int c, int[][] dp) {
-
-        // already computed
         if (dp[r][c] != 0) {
             return dp[r][c];
         }
 
         int curLongest = 1;
-
-        for (int[] d : dirs) {
+        for (int[] d : DIRS) {
             int nr = r + d[0];
             int nc = c + d[1];
 
@@ -61,13 +57,51 @@ class Solution {
                 continue;
             }
 
-            // increasing condition
             if (matrix[nr][nc] > matrix[r][c]) {
                 curLongest = Math.max(curLongest, 1 + dfs(matrix, nr, nc, dp));
             }
         }
         dp[r][c] = curLongest;
-
         return curLongest;
     }
 }
+
+
+// brute force
+// class Solution {
+
+//     static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+//     public int longestIncreasingPath(int[][] matrix) {
+//         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+//             return 0;
+//         }
+
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+
+//         int res = 0;
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 res = Math.max(res, dfs(matrix, i, j));
+//             }
+//         }
+//         return res;
+//     }
+
+//     private int dfs(int[][] matrix, int i, int j) {
+//         int res = 0;
+//         for (int[] d : DIRS) {
+//             int x = i + d[0];
+//             int y = j + d[1];
+//             if (x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length && matrix[i][j] < matrix[x][y]) {
+//                 res = Math.max(res, dfs(matrix, x, y));
+//             }
+//         }
+//         return res + 1;
+//     }
+// }
+/**
+3 4
+3 2
+ */
