626963c15e4dfbe5f5f5e87c628c5f7ab37154ca
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-04 17:14:38 -0700

    Time: 0 ms (100%), Space: 48 MB (37.86%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,57 +1,29 @@
+// Solution 2: 2 Pointers
 class Solution {
     public int trap(int[] height) {
         if (height == null || height.length < 3) {
             return 0;
         }
+        int left = 0, right = height.length - 1;
+        int leftMax = 0; 
+        int rightMax = 0;
         int totalWater = 0;
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = 0; i < height.length; i++) {
-            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
-                int valleyIdx = stack.pop();
-                if (stack.isEmpty()) {
-                    break;
-                }
-                int leftWallIdx = stack.peek();
-                int rightWallIdx = i;
-                int width = rightWallIdx - leftWallIdx - 1;
-                int boundedHeight = Math.min(height[leftWallIdx], height[rightWallIdx]) - height[valleyIdx];
-                totalWater += boundedHeight * width;           
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                totalWater += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                totalWater += rightMax - height[right];
+                right--;
             }
-            stack.push(i);
         }
         return totalWater;
     }
 }
 
-
-
-// class Solution {
-//     public int trap(int[] height) {
-//         if (height == null || height.length == 0) {
-//             return 0;
-//         }        
-//         int left = 0, right = height.length - 1;
-//         int leftMax = 0;
-//         int rightMax = 0;
-//         int water = 0;
-
-//         while (left < right) {
-//             if (height[left] < height[right]) {
-//                 leftMax = Math.max(leftMax, height[left]);
-//                 water += leftMax - height[left];
-//                 left++;
-//             } else {
-//                 rightMax = Math.max(rightMax, height[right]);
-//                 water += rightMax - height[right];
-//                 right--;
-//             }
-//         }
-//         return water;
-//     }
-// }
-
-
-
+// Solution 1: DP:  Find both side max height for each idx
 // class Solution {
 //     public int trap(int[] height) {
 //         if (height == null || height.length == 0) {
@@ -76,5 +48,31 @@ class Solution {
 //             res += (Math.min(rightToLeftMax[i], leftToRightMax[i]) - height[i]);
 //         }
 //         return res;
+//     }
+// }
+
+// Decreasing monotonic stack
+// class Solution {
+//     public int trap(int[] height) {
+//         if (height == null || height.length < 3) {
+//             return 0;
+//         }
+//         int totalWater = 0;
+//         Deque<Integer> stack = new ArrayDeque<>();
+//         for (int i = 0; i < height.length; i++) {
+//             while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+//                 int valleyIdx = stack.pop();
+//                 if (stack.isEmpty()) {
+//                     break;
+//                 }
+//                 int leftWallIdx = stack.peek();
+//                 int rightWallIdx = i;
+//                 int width = rightWallIdx - leftWallIdx - 1;
+//                 int boundedHeight = Math.min(height[leftWallIdx], height[rightWallIdx]) - height[valleyIdx];
+//                 totalWater += boundedHeight * width;           
+//             }
+//             stack.push(i);
+//         }
+//         return totalWater;
 //     }
 // }
