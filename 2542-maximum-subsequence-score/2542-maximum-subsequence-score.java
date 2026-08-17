840419e24699853fa8efc51190457388dfb5912c
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-17 14:46:23 -0700

    Time: 79 ms (60.84%), Space: 102.5 MB (55.87%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -1,38 +1,69 @@
+
 class Solution {
-
-    private static class Pair{
-        int n1;
-        int n2;
-
-        Pair(int x, int y) {
-            this.n1 = x;
-            this.n2 = y;
-        }
-    }
     public long maxScore(int[] nums1, int[] nums2, int k) {
-        Pair[] pairs = new Pair[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            pairs[i] = new Pair(nums1[i], nums2[i]);
+        int n = nums1.length;
+        int[][] pairs = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            pairs[i][0] = nums1[i];
+            pairs[i][1] = nums2[i];
         }
-        // sort by decending of nums2
-        Arrays.sort(pairs, (a, b) -> Integer.compare(b.n2, a.n2));
-
+        // decende the pairs based on pairs[i][1] (nums2)
+        Arrays.sort(pairs, (a, b) -> b[1] - a[1]);
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         long sum = 0;
         long max = Long.MIN_VALUE;
-        for (Pair pair : pairs) {
-            sum += pair.n1;
-            minHeap.offer(pair.n1);
+
+        for (int i = 0; i < n; i++) {
+            sum += pairs[i][0];
+            minHeap.offer(pairs[i][0]);
 
             if (minHeap.size() > k) {
                 sum -= minHeap.poll();
             }
 
             if (minHeap.size() == k) {
-                max = Math.max(max, sum * pair.n2);
+                max = Math.max(max, sum * pairs[i][1]);
             }
         }
         return max;
     }
 }
+
+// class Solution {
+
+//     private static class Pair{
+//         int n1;
+//         int n2;
+
+//         Pair(int x, int y) {
+//             this.n1 = x;
+//             this.n2 = y;
+//         }
+//     }
+//     public long maxScore(int[] nums1, int[] nums2, int k) {
+//         Pair[] pairs = new Pair[nums1.length];
+//         for (int i = 0; i < nums1.length; i++) {
+//             pairs[i] = new Pair(nums1[i], nums2[i]);
+//         }
+//         // sort by decending of nums2
+//         Arrays.sort(pairs, (a, b) -> Integer.compare(b.n2, a.n2));
+
+//         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+//         long sum = 0;
+//         long max = Long.MIN_VALUE;
+//         for (Pair pair : pairs) {
+//             sum += pair.n1;
+//             minHeap.offer(pair.n1);
+
+//             if (minHeap.size() > k) {
+//                 sum -= minHeap.poll();
+//             }
+
+//             if (minHeap.size() == k) {
+//                 max = Math.max(max, sum * pair.n2);
+//             }
+//         }
+//         return max;
+//     }
+// }
 
