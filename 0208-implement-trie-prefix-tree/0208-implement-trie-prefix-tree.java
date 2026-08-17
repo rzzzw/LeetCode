81b4cdebe9cff84a1a0137d5be241afd881c7819
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-16 20:27:36 -0700

    Time: 31 ms (86.65%), Space: 61.8 MB (74.17%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,53 +1,60 @@
 class Trie {
+
     private static class TrieNode {
         TrieNode[] children = new TrieNode[26];
-        boolean isWord;
+        boolean isWord; 
     }
 
-    private TrieNode root;
-
-    public Trie() {
-        root = new TrieNode();
-    }
+    TrieNode root = new TrieNode();
 
     public void insert(String word) {
         TrieNode cur = root;
         for (char c : word.toCharArray()) {
             int idx = c - 'a';
-            if (cur.children[idx] == null) {
+            if(cur.children[idx] == null) {
                 cur.children[idx] = new TrieNode();
-            }
+            } 
             cur = cur.children[idx];
         }
         cur.isWord = true;
     }
 
     public boolean search(String word) {
-        TrieNode node = findNode(word);
-        return node != null && node.isWord;
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (cur.children[idx] == null) {
+                return false;
+            }
+            cur = cur.children[idx];
+        }
+        return cur.isWord;
     }
 
     public boolean startsWith(String prefix) {
-        TrieNode node = findNode(prefix);
-        return node != null;
-    }
-
-    private TrieNode findNode(String s) {
         TrieNode cur = root;
-        for (char c : s.toCharArray()) {
+        for (char c : prefix.toCharArray()) {
             int idx = c - 'a';
             if (cur.children[idx] == null) {
-                return null;
+                return false;
             }
             cur = cur.children[idx];
-        }        
-        return cur;
+        }
+        return true;
     }
+
+    // public TrieNode findStr(String str) {
+    //     TrieNode cur = root;
+    //     for (char c : str.toCharArray()) {
+    //         int idx = c - 'a';
+    //         if (cur.children[idx] == null) {
+    //             return null;
+    //         }
+    //         cur = cur.children[idx];
+    //     }
+    //     return cur;        
+    // }
 }
-
-
-
-
 
 /**
 Complexity Analysis
