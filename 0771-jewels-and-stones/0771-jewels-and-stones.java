bbfc789ea91e548daa725c8e8cc2a45b4e6d509f
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-18 17:23:48 -0700

    Time: 1 ms (63.28%), Space: 43.3 MB (36.96%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        if (jewels == null || jewels.length() == 0 || stones == null || stones.length() == 0) {
+            return 0;
+        }
+        Set<Character> set = new HashSet<>();
+        for (char c : jewels.toCharArray()) {
+            set.add(c);
+        }
+        int count = 0;
+        for (char c : stones.toCharArray()) {
+            if (set.contains(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
