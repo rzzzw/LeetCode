705a999a7ba4b7fec780c21c5e7f733da012cef6
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-19 12:20:27 -0700

    Time: 2 ms (57.1%), Space: 46.5 MB (49.32%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,12 +3,15 @@ class Solution {
         if (ransomNote == null || ransomNote.length() == 0) {
             return true;
         }
+        if (magazine == null || magazine.length() == 0) {
+            return false;
+        }
         int[] map = new int[26];
         for (char c : magazine.toCharArray()) {
-            map[c - 'a']++;
+            map[c -'a']++;
         }
         for (char c : ransomNote.toCharArray()) {
-            if (map[c - 'a'] == 0) {
+            if (map[c -'a'] == 0) {
                 return false;
             }
             map[c - 'a']--;
