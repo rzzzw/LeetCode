8d7d8ed0799f95374a8e848635e1e8d06f976ce4
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-31 11:38:54 -0700

    Time: 1 ms (99.91%), Space: 52.8 MB (64.07%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -17,6 +17,10 @@ Idea:
     - Pointer pA starts at headA
     - Pointer pB starts at headB
     - When a pointer reaches the end, jump it to the other list’s head
+
+pA from headA: a1 a2 c1 c2 c3 b1 b2 b3 *c1 c2 c3 
+pB from headB: b1 b2 b3 c1 c2 c3 a1 a2 *c1 c2 c3 
+
 Eventually:
     - They either meet at the intersection
     - Or both reach null
@@ -25,6 +29,7 @@ Key logic:
     Each pointer travels:  lengthA + lengthB. So they are guaranteed to be aligned on the second pass.
 
  */
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) {
@@ -34,8 +39,19 @@ public class Solution {
         ListNode pB = headB;
         while (pA != pB) {
             pA = (pA == null) ? headB : pA.next;
+            // if (pA == null) {
+            //     pA = headB;
+            // } else {
+            //     pA = pA.next;
+            // }
+
             pB = (pB == null) ? headA : pB.next;
+            // if (pB == null) {
+            //     pB = headA;
+            // } else {
+            //     pB = pB.next;
+            // }
         }
-        return pB;
+        return pA;
     }
 }
