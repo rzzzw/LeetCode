7e28b9fb2d4a90327e20d955636c8fba85b74021
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-31 12:03:49 -0700

    Time: 1 ms (85.75%), Space: 44.4 MB (80%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -6,30 +6,58 @@ n = 0 -> [""]
 backtracking with constrains
  */
 
+// class Solution {
+//     public List<String> generateParenthesis(int n) {
+//         List<String> res = new ArrayList<>();
+//         helper(n, 0, 0, new StringBuilder(), res);
+//         return res;
+//     }
+
+//     private void helper(int n, int leftUsed, int rightUsed, StringBuilder path, List<String> res) {
+//         if (path.length() == 2 * n) {
+//             res.add(path.toString());
+//             return;
+//         }
+
+//         if (leftUsed < n) {
+//             path.append('(');
+//             helper(n, leftUsed+1, rightUsed, path, res);
+//             path.deleteCharAt(path.length() - 1);
+//         }
+
+//         if (rightUsed < n && rightUsed < leftUsed) {
+//             path.append(')');
+//             helper(n, leftUsed, rightUsed+1, path, res);
+//             path.deleteCharAt(path.length() - 1);
+//         }
+//     }
+// }
+
+
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        helper(n, 0, 0, new StringBuilder(), res);
+        if (n == 0) {
+            return res;
+        }
+        backtracking(n, n, n, new StringBuilder(), res);
         return res;
     }
 
-    private void helper(int n, int leftUsed, int rightUsed, StringBuilder path, List<String> res) {
-        if (path.length() == 2 * n) {
-            res.add(path.toString());
+    private void backtracking(int n, int leftHas, int rightHas, StringBuilder sb, List<String> res) {
+        if (sb.length() == n * 2) { //leftHas == 0 && rightHas == 0
+            res.add(sb.toString());
             return;
         }
-
-        if (leftUsed < n) {
-            path.append('(');
-            helper(n, leftUsed+1, rightUsed, path, res);
-            path.deleteCharAt(path.length() - 1);
+        if (leftHas > 0) {
+            sb.append('(');
+            backtracking(n, leftHas - 1, rightHas, sb, res);
+            sb.deleteCharAt(sb.length() - 1);
         }
-
-        if (rightUsed < n && rightUsed < leftUsed) {
-            path.append(')');
-            helper(n, leftUsed, rightUsed+1, path, res);
-            path.deleteCharAt(path.length() - 1);
+        if (rightHas > 0 && rightHas > leftHas) {
+            sb.append(')');
+            backtracking(n, leftHas, rightHas - 1, sb, res);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
-
