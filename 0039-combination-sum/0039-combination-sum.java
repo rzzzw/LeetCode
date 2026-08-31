49bf70f5d0e9d4ad58523543eb73edd1b75de756
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-08-31 15:31:14 -0700

    Time: 2 ms (95.5%), Space: 45.5 MB (92.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,34 +2,62 @@
 binary-decision backtracking formulation
  */
 
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if (candidates == null || candidates.length == 0) {
+//             return res;
+//         }
+//         backtrack(candidates, target, 0, new ArrayList<>(), res);
+//         return res;
+//     }
+
+//     private void backtrack(int[] candidates, int remains, int idx, List<Integer> cur, List<List<Integer>> res) {
+//         if (remains == 0) {
+//             res.add(new ArrayList<>(cur));
+//             return;
+//         }
+
+//         if (remains < 0 || idx == candidates.length) {
+//             return;
+//         }
+
+//         // 加
+//         if (candidates[idx] <= remains) {
+//             cur.add(candidates[idx]);
+//             backtrack(candidates, remains - candidates[idx], idx, cur, res);  // ❌ idx + 1
+//             cur.remove(cur.size() - 1);
+//         }
+
+//         // 不加
+//         backtrack(candidates, remains, idx + 1, cur, res);
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         if (candidates == null || candidates.length == 0) {
             return res;
         }
-        backtrack(candidates, target, 0, new ArrayList<>(), res);
+        backtrack(candidates, 0, target, new ArrayList<>(), res);
         return res;
     }
 
-    private void backtrack(int[] candidates, int remains, int idx, List<Integer> cur, List<List<Integer>> res) {
-        if (remains == 0) {
-            res.add(new ArrayList<>(cur));
+    private void backtrack(int[] candidates, int idx, int remain, List<Integer> cur, List<List<Integer>> res) {
+        if (idx >= candidates.length) {
+            if (remain == 0) {
+                res.add(new ArrayList<>(cur));
+            }
             return;
         }
-
-        if (remains < 0 || idx == candidates.length) {
-            return;
-        }
-
         // 加
-        if (candidates[idx] <= remains) {
+        if (candidates[idx] <= remain) {
             cur.add(candidates[idx]);
-            backtrack(candidates, remains - candidates[idx], idx, cur, res);  // ❌ idx + 1
+            backtrack(candidates, idx, remain - candidates[idx], cur, res);
             cur.remove(cur.size() - 1);
         }
 
-        // 不加
-        backtrack(candidates, remains, idx + 1, cur, res);
+        backtrack(candidates, idx + 1, remain, cur, res);
     }
 }
