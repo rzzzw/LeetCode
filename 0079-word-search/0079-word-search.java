f120d99bbedf46c46d8fb310bf8e84d8eba87c3d
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-09-01 12:20:08 -0700

    Time: 174 ms (21.96%), Space: 43.2 MB (17.47%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,48 +1,48 @@
-class Solution {
-    private static final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+// class Solution {
+//     private static final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-    public boolean exist(char[][] board, String word) {
-        if (board == null || board.length == 0 || board[0].length == 0) {
-            return false;
-        }
-        int row = board.length;
-        int col = board[0].length;
-        for (int r = 0; r < row; r++) {
-            for (int c = 0; c < col; c++) {
-                if (board[r][c] == word.charAt(0)) {
-                    if (dfs(board, word, 0, r, c)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
+//     public boolean exist(char[][] board, String word) {
+//         if (board == null || board.length == 0 || board[0].length == 0) {
+//             return false;
+//         }
+//         int row = board.length;
+//         int col = board[0].length;
+//         for (int r = 0; r < row; r++) {
+//             for (int c = 0; c < col; c++) {
+//                 if (board[r][c] == word.charAt(0)) {
+//                     if (dfs(board, word, 0, r, c)) {
+//                         return true;
+//                     }
+//                 }
+//             }
+//         }
+//         return false;
+//     }
 
-    private boolean dfs(char[][] board, String word, int idx, int r, int c) {
-        if (idx == word.length()) {
-            return true;
-        }
-        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) {
-            return false;
-        }
-        if (board[r][c] != word.charAt(idx)) {
-            return false;
-        }
+//     private boolean dfs(char[][] board, String word, int idx, int r, int c) {
+//         if (idx == word.length()) {
+//             return true;
+//         }
+//         if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) {
+//             return false;
+//         }
+//         if (board[r][c] != word.charAt(idx)) {
+//             return false;
+//         }
 
-        char temp = board[r][c];
-        board[r][c] = '#';
+//         char temp = board[r][c];
+//         board[r][c] = '#';
 
-        for (int[] d : DIRS) {
-            if (dfs(board, word, idx + 1, r + d[0], c + d[1])) {
-                board[r][c] = temp;
-                return true;
-            }
-        }
-        board[r][c] = temp;
-        return false;
-    }
-}
+//         for (int[] d : DIRS) {
+//             if (dfs(board, word, idx + 1, r + d[0], c + d[1])) {
+//                 board[r][c] = temp;
+//                 return true;
+//             }
+//         }
+//         board[r][c] = temp;
+//         return false;
+//     }
+// }
 
 
 
@@ -58,3 +58,46 @@ Space Complexity: O(L)
     We modify the board in-place, so no extra matrix
 
 */
+class Solution {
+    private static final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0].length == 0){
+            return false;
+        }
+        int rows = board.length;
+        int cols = board[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, int r, int c, String word, int idx) {
+        if (idx == word.length()) {
+            return true;
+        }
+
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length) {
+            return false;
+        }
+
+        if (board[r][c] !=  word.charAt(idx)) {
+            return false;
+        }
+
+        char origin = board[r][c];
+        board[r][c] = '#';
+        for (int[] d : DIRS) {
+            if (dfs(board, r + d[0], c + d[1], word, idx + 1)) {
+                // board[r][c] = origin;
+                return true;
+            }
+        }
+        board[r][c] = origin;
+        return false;
+    }
+}
