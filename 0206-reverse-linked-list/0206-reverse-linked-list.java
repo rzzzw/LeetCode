27ae9e4c92d849fbfc1eec7bb1865cb1bde86d70
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-09-14 15:46:07 -0700

    Time: 0 ms (100%), Space: 44.5 MB (14.05%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,37 +9,42 @@
  * }
  */
 
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode cur = head;
-        ListNode pre = null;
-
-        while (cur != null) {
-            ListNode tmp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = tmp;
-        }
-        return pre;
-    }
-}
-
-
-
 // class Solution {
 //     public ListNode reverseList(ListNode head) {
 //         if (head == null || head.next == null) {
 //             return head;
 //         }
-//         ListNode newHead = reverseList(head.next);
-//         head.next.next = head;
-//         head.next = null;
-//         return newHead;
+//         ListNode cur = head;
+//         ListNode pre = null;
+
+//         while (cur != null) {
+//             ListNode tmp = cur.next;
+//             cur.next = pre;
+//             pre = cur;
+//             cur = tmp;
+//         }
+//         return pre;
 //     }
 // }
+
+/**
+Recursion:
+Trust recursion to reverse everything after head. Your only job is to attach head to the end.
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+
+
+
 
 
 /**
