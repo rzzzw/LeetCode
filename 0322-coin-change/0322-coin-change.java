9c896ec4e012557116c63717a220740ae4c10d91
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-09-16 20:22:33 -0700

    Time: 15 ms (83.79%), Space: 46.5 MB (52.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,22 @@
-public class Solution {
+class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) return 0;
-        int max = amount + 1; 
+        if (amount == 0){
+            return 0;
+        }
+        int max = amount + 1;
         int[] dp = new int[amount + 1]; // dp[i] = minimum coins to make up amount i
         Arrays.fill(dp, max);
         dp[0] = 0; // to make amount = 0, we need 0 coins.
-        for (int i = 1; i <= amount; i++) {
+        for(int i = 1; i <= amount; i++) {
             for (int c : coins) {
                 if (i - c >= 0) {
                     dp[i] = Math.min(dp[i], dp[i - c] + 1);
-                }         
+                }
             }
         }
         return dp[amount] == max ? -1 : dp[amount];
     }
 }
-
 
 
 /**
