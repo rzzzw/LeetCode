55000664352982d4233c86f9a26761967169cd80
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-09-18 12:24:20 -0700

    Time: 3 ms (6.85%), Space: 94.4 MB (51.49%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,10 +1,11 @@
+
 class Solution {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length <= 1) {
             return 0;
         }
         int minPrice = Integer.MAX_VALUE;
-        int max = 0;        
+        int max = 0;
         for (int i = 0; i < prices.length; i++) {
             minPrice = Math.min(minPrice, prices[i]);
             max = Math.max(max, prices[i] - minPrice);
@@ -12,4 +13,3 @@ class Solution {
         return max;
     }
 }
-
