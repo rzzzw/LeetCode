4eca2825800a6b6534b07bc85fddd0c2f2294a00
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-09-18 16:18:19 -0700

    Time: 3 ms (17.7%), Space: 47.8 MB (38.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,25 +4,30 @@
 // [slow ... i-1]   → all zeros
 // [i ... end]      → unexplored
 //  */
-// class Solution {
-//     public void moveZeroes(int[] nums) {
-//         if (nums == null || nums.length == 0) {
-//             return;
-//         }
-//         int slow = 0;
-//         for (int fast = 0; fast < nums.length; fast++) {
-//             if (nums[fast] != 0) {
-//                 swap(nums, fast, slow);
-//                 slow++;
-//             }
-//         }
-//     }
-//     private void swap(int[] nums, int i, int j) {
-//         int temp = nums[i];
-//         nums[i] = nums[j];
-//         nums[j] = temp;
-//     }
-// }
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                swap(nums, slow, fast);
+                slow++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+
+
 
 /**
 Approach 2: Overwrite + Fill Zeros
@@ -30,18 +35,19 @@ Step 1: Copy non-zero elements forward
 Step 2: Fill the rest with zeros
  */
 
-class Solution {
-    public void moveZeroes(int[] nums) {
-        int idx = 0;
-        for (int num : nums) {
-            if (num != 0) {
-                nums[idx] = num;
-                idx++;
-            }
-        }
-        while (idx < nums.length) {
-            nums[idx] = 0;
-            idx++;
-        }
-    }
-}
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int idx = 0;
+//         for (int num : nums) {
+//             if (num != 0) {
+//                 nums[idx] = num;
+//                 idx++;
+//             }
+//         }
+//         while (idx < nums.length) {
+//             nums[idx] = 0;
+//             idx++;
+//         }
+//     }
+// }
+
