6d9810bcbc408c3efffe0c930d346fb2a902364d
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-09-21 16:05:53 -0700

    Time: 7 ms (94.61%), Space: 71 MB (58.74%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,28 +1,21 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
         int rows = matrix.length;
         int cols = matrix[0].length;
-        int[][] dp = new int[rows][cols];
+        int[][] dp = new int[rows + 1][cols + 1];
         int maxSide = 0;
 
-        for (int r = 0; r < rows; r++) {
-            dp[r][0] = matrix[r][0] - '0';
-            maxSide = Math.max(maxSide, dp[r][0]);
-        }
-
-        for (int c = 0; c < cols; c++) {
-            dp[0][c] = matrix[0][c] - '0';
-            maxSide = Math.max(maxSide, dp[0][c]);
-        }
-
-        for (int r = 1; r < rows; r++) {
-            for (int c = 1; c < cols; c++) {
-                if (matrix[r][c] == '1') {
-                    dp[r][c] = 1 + Math.min(dp[r - 1][c], Math.min(dp[r - 1][c - 1], dp[r][c - 1]));
+        for (int r = 1; r <= rows; r++) {
+            for (int c = 1; c <= cols; c++) {
+                if (matrix[r - 1][c - 1] == '1') {
+                    dp[r][c] = Math.min(Math.min(dp[r - 1][c - 1], dp[r - 1][c]), dp[r][c - 1]) + 1;
                     maxSide = Math.max(maxSide, dp[r][c]);
                 }
             }
-        } 
+        }
         return maxSide * maxSide;
     }
 }
