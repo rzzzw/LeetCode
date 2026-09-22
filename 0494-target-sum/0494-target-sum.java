f107f57c8b083c4a4c09547f25dee4efa7950588
Wei Zhu <91703172+rzzzw@users.noreply.github.com>
2026-09-22 13:00:22 -0700

    Time: 661 ms (5.01%), Space: 43 MB (78.72%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] count = new int[]{0};
+        helper(nums, target, 0, 0, count);
+        return count[0];
+    }
+    private void helper(int[] nums, int target, int idx, int sum, int[] count){
+        if (idx == nums.length) {
+            if (sum == target) {
+                count[0]++;
+            }
+            return;
+        }
+
+        // "-"
+        sum += nums[idx] * (-1);
+        helper(nums, target, idx + 1, sum, count);
+        sum -= nums[idx] * (-1);
+
+        // "+"
+        sum += nums[idx];
+        helper(nums, target, idx + 1, sum, count);
+
+    }
+
+}
